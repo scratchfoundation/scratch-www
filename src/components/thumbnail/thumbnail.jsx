a02c73ddf29c3d26a11ff586b9f92993b55eb989
Ray Schamp <ray@scratch.mit.edu>
2015-09-08 14:56:28 -0400

    Add carousel component

---
+++ src/components/thumbnail/thumbnail.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+require('./thumbnail.scss');
+
+module.exports = React.createClass({
+    propTypes: {
+        src: React.PropTypes.string
+    },
+    getDefaultProps: function () {
+        return {
+            href: '/projects/1000/',
+            title: 'Example Project',
+            src: 'http://www.lorempixel.com/144/108/',
+            extra: 'by raimondious'
+        };
+    },
+    render: function () {
+        var className = this.props.className + " thumbnail";
+        return (
+            <div className={className}>
+                <a className="thumbnailImage" href={this.props.href}>
+                    <img src={this.props.src} />
+                </a>
+                <span className="thumbnailTitle"><a href={this.props.href}>{this.props.title}</a></span>
+                <span className="thumbnailExtra">{this.props.extra}</span>
+            </div>
+        );
+    }
+});
