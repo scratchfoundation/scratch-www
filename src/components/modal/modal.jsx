27fc9ff012060e0dc9eec4fbe8945cc430c8d453
Ray Schamp <ray@scratch.mit.edu>
2015-09-09 22:15:16 -0400

    Add a modal component, extending react-modal
    Also fix some z-index quirks

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var Modal = require('react-modal');
+
+require('./modal.scss');
+
+
+module.exports = React.createClass({
+    statics: {
+        setAppElement: Modal.setAppElement
+    },
+    requestClose: function() {
+        return this.refs.modal.portal.requestClose();
+    },
+    render: function() {
+        return (
+            <Modal ref='modal' {... this.props}>
+                <div className='modal-close' onClick={this.requestClose}></div>
+                {this.props.children}
+            </Modal>
+        );
+    }
+});
