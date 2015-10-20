a7255c49b2a7b53022f154448f4d52951f9812b5
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-10-20 10:51:33 -0400

    Add GA tracking and optimize open graph tags

---
+++ server/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // Search and metadata
+    title: 'Imagine, Program, Share',
+    description:
+        'Scratch is a free programming language and online community ' +
+        'where you can create your own interactive stories, games, ' +
+        'and animations.',
+
+    // Open graph
+    og_image: 'https://scratch.mit.edu/images/og_image.jpg',
+    og_image_type: 'image/jpeg',
+    og_image_width: 986,
+    og_image_height: 860,
+
+    // Analytics & Monitoring
+    ga_tracker: process.env.GA_TRACKER || ''
+};
