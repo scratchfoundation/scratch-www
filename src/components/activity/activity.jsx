39550d805a30767dac47c3c187d5350f01fc95be
Ray Schamp <ray@scratch.mit.edu>
2015-10-23 17:41:50 -0400

    Fix #152: Don't output activity without a message

--- src/components/activity/activity.jsx
+++ src/components/activity/activity.jsx
@@ -38,20 +38,21 @@ var Activity = React.createClass({
                         var actionDate = new Date(item.datetime_created + 'Z');
                         var activityMessageHTML = '<a href=' + actorProfileUrl + '>' +
                             item.actor.username + '</a>' + item.message;
-                            
-                        return (
-                            <li key={item.pk}>
-                                <a href={actorProfileUrl}>
-                                    <img src={item.actor.thumbnail_url} width="34" height="34" />
-                                    <p dangerouslySetInnerHTML={{__html: activityMessageHTML}}></p>
-                                    <p>
-                                        <span className="stamp">
-                                            <FormattedRelative value={actionDate} />
-                                        </span>
-                                    </p>
-                                </a>
-                            </li>
-                        );
+                        if (item.message.replace(/\s/g, '')) {
+                            return (
+                                <li key={item.pk}>
+                                    <a href={actorProfileUrl}>
+                                        <img src={item.actor.thumbnail_url} width="34" height="34" />
+                                        <p dangerouslySetInnerHTML={{__html: activityMessageHTML}}></p>
+                                        <p>
+                                            <span className="stamp">
+                                                <FormattedRelative value={actionDate} />
+                                            </span>
+                                        </p>
+                                    </a>
+                                </li>
+                            );
+                        }
                     })}
                 </ul>
             </Box>
