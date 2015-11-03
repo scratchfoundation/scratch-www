f8d4bf48c19d9bc4f769257df5a9172eb17bceff
Hannah Cole <hlcolani@gmail.com>
2015-11-03 15:58:25 -0500

    added credits page

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -8,85 +8,103 @@ var About = React.createClass({
     render: function () {
         return (
             <div className="inner">
-                <h1> <img src="//cdn.scratch.mit.edu/scratchr2/static/__
-                2ed432bc0488a9a3d506f1bc0f4afd45__//images/help/cat1.png"> </img> About Scratch</h1>
+                <h1>
+                    <img src="/images/cat1.png" />
+                    About Scratch
+                </h1>
+
                 <div className="intro">
-                	<p>With Scratch, you can program your own interactive stories,
+                    <p>With Scratch, you can program your own interactive stories,
                      games, and animations — and share your creations with others in the online community. </p>
 
-					<p>Scratch helps young people learn to think creatively, reason systematically, 
+                    <p>Scratch helps young people learn to think creatively, reason systematically,
                     and work collaboratively — essential skills for life in the 21st century.</p>
 
-					<p>Scratch is a project of the Lifelong Kindergarten Group
+                    <p>Scratch is a project of the Lifelong Kindergarten Group
                      at the MIT Media Lab. It is provided free of charge.</p>
-                    <p> <a href = "/parents/"> INFO FOR PARENTS </a> |<a href = "/educators/"> INFO FOR EDUCATORS </a></p>
-				</div>
-				<div className="video">
-					<iframe src="https://player.vimeo.com/video/65583694?
-                    title=0&byline=0&portrait=0" width="445" height="250" frameborder="0" 
-                    webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-				</div>
+                    <p>
+                        <a href = "/parents/"> INFO FOR PARENTS </a> |
+                        <a href = "/educators/"> INFO FOR EDUCATORS </a>
+                    </p>
+                </div>
+                <div className="video">
+                    <iframe src="https://player.vimeo.com/video/65583694?
+                    title=0&byline=0&portrait=0" width="445" height="250" frameborder="0"
+                    webkitallowfullscreen mozallowfullscreen allowfullscreen />
+                </div>
                 <div className="information">
                     <div className="leftcolumn">
                         <h2>Who Uses Scratch?</h2>
-                        <img src="/images/about/who-uses-scratch.jpg"></img>
-                        <p>Scratch is designed especially for ages 8 to 16, but 
-                        is used by people of all ages. Millions of people are creating 
-                        Scratch projects in a wide variety of settings, including homes, schools, museums, 
+                        <img src="/images/about/who-uses-scratch.jpg" />
+                        <p>Scratch is designed especially for ages 8 to 16, but
+                        is used by people of all ages. Millions of people are creating
+                        Scratch projects in a wide variety of settings, including homes, schools, museums,
                         libraries, and community centers.</p>
                         <h2>Around the World</h2>
-                        <img src="/images/about/around-the-world.png"></img>
-                        <p>Scratch is used in more than 150 different countries 
-                        and available in more than 40 languages. To change languages, click the menu at 
-                        the bottom of the page. Or, in the Project Editor, click the globe at the top of the page. 
-                        To add or improve a translation, see the <a href="http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch">
-                        translation</a> page.</p>
+                        <img src="/images/about/around-the-world.png" />
+                        <p>Scratch is used in more than 150 different countries
+                        and available in more than 40 languages. To change languages, click the menu at
+                        the bottom of the page. Or, in the Project Editor, click the globe at the top of the page.
+                        To add or improve a translation, see the
+                            <a href="http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch">
+                        translation</a>
+                             page.</p>
                         <h2>Quotes</h2>
-                        <img src="images/about/quotes.gif"></img>
-                        <p>The Scratch Team has received many emails from youth, parents, and educators 
-                        expressing thanks for Scratch. Want to see what people are saying? You can 
+                        <img src="images/about/quotes.gif" />
+                        <p>The Scratch Team has received many emails from youth, parents, and educators
+                        expressing thanks for Scratch. Want to see what people are saying? You can
                         read a collection of the <a href="/info/quotes">quotes</a> we&#39;ve received.</p>
                         <h2>Learn More About Scratch</h2>
                         <div className="moreinfolist">
                         <ul>
-                            <li><a href="/help">Scratch Help Page</a></li>
-                            <li><a href="/info/faq">Frequently Asked Questions</a></li>
-                            <li><a href="/parents">Information for Parents</a></li>
-                            <li><a href="/credits">Scratch Credits</a></li>
+                            <li>
+                                <a href="/help">Scratch Help Page</a>
+                            </li>
+                            <li>
+                                <a href="/info/faq">Frequently Asked Questions</a>
+                            </li>
+                            <li>
+                                <a href="/parents">Information for Parents</a>
+                            </li>
+                            <li>
+                                <a href="/credits">Scratch Credits</a>
+                            </li>
                         </ul>
                         </div>
+
+
                      </div>
                      <div className="rightcolumn">
                         <h2>Learn to Code, Code to Learn</h2>
                         <iframe src="https://embed-ssl.ted.com/talks/mitch_resnick_let_s_teach_kids_to_code.html"
                          scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-                        <p>The ability to code computer programs is an important part of literacy in today’s 
-                        society. When people learn to code in Scratch, they learn important strategies for 
+                        <p>The ability to code computer programs is an important part of literacy in today’s
+                        society. When people learn to code in Scratch, they learn important strategies for
                         solving problems, designing projects, and communicating ideas.</p>
                         <h2>Scratch in Schools</h2>
-                        <img src="/images/about/scratch-in-schools.jpg"></img>
-                        <p>Students are learning with Scratch at all levels (from elementary school to college) 
-                        and across disciplines (such as math, computer science, language arts, social studies). 
-                        Educators share stories, exchange resources, ask questions, and find people on the 
+                        <img src="/images/about/scratch-in-schools.jpg" />
+                        <p>Students are learning with Scratch at all levels (from elementary school to college)
+                        and across disciplines (such as math, computer science, language arts, social studies).
+                        Educators share stories, exchange resources, ask questions, and find people on the
                         <a href="http://scratched.gse.harvard.edu/">ScratchEd website</a>.</p>
                         <h2>Research</h2>
-                        <img src="/images/about/research-remix.png"></img>
-                        <p>The MIT Scratch Team and collaborators are researching how people use and 
-                        learn with Scratch (for an introduction, see 
+                        <img src="/images/about/research-remix.png" />
+                        <p>The MIT Scratch Team and collaborators are researching how people use and
+                        learn with Scratch (for an introduction, see
                             <a href="http://web.media.mit.edu/~mres/papers/Scratch-CACM-final.pdf">
-                            Scratch: Programming for All</a>). 
-                        Find out more about Scratch <a href="/info/research">research</a> and 
+                            Scratch: Programming for All</a>).
+                        Find out more about Scratch <a href="/info/research">research</a> and
                         <a href="/statistics">statistics</a> about Scratch.</p>
                         <h2>Support and Funding</h2>
-                        <p>The Scratch project, initiated in 2003, has received generous support 
-                        from the National Science Foundation (grants 0325828, 1002713, 1027848, 1019396), 
-                        Intel Foundation, Microsoft, MacArthur Foundation, LEGO Foundation, 
-                        Code-to-Learn Foundation, Google, Dell, Fastly, Inversoft, and 
-                        MIT Media Lab research consortia. If you&#39;d like to support Scratch, 
+                        <p>The Scratch project, initiated in 2003, has received generous support
+                        from the National Science Foundation (grants 0325828, 1002713, 1027848, 1019396),
+                        Intel Foundation, Microsoft, MacArthur Foundation, LEGO Foundation,
+                        Code-to-Learn Foundation, Google, Dell, Fastly, Inversoft, and
+                        MIT Media Lab research consortia. If you&#39;d like to support Scratch,
                         please see our <a href="https://secure.donationpay.org/codetolearn/">
-                        donate page</a>, 
+                        donate page</a>,
                         or contact us at donate@scratch.mit.edu.</p>
-                    </div> 
+                    </div>
                 </div>
             </div>
         );
--- src/views/help/help.jsx
+++ src/views/help/help.jsx
@@ -22,9 +22,9 @@ var View = React.createClass({
                             </div>
                             <div className="get-started">
                                 <iframe src="//player.vimeo.com/video/80961102?title=0&amp;
-                                byline=0&amp;portrait=0;hd_off=1&amp;" 
-                                width="170" height="128" frameborder="0" webkitallowfullscreen="" 
-                                mozallowfullscreen="" 
+                                byline=0&amp;portrait=0;hd_off=1&amp;"
+                                width="170" height="128" frameborder="0" webkitallowfullscreen=""
+                                mozallowfullscreen=""
                                 allowfullscreen=""></iframe>
                                 <p><a href="https://vimeo.com/80961102">Watch the Getting Started video</a></p>
                             </div>
@@ -36,23 +36,29 @@ var View = React.createClass({
                             <div className="guides">
                                 <div className="scratch-guide">
                                     <img src="/images/help/guide.png"></img>
-                                    <h4><a href="https://cdn.scratch.mit.edu/scratchr2/static/__
+                                    <h4>
+                                        <a href="https://cdn.scratch.mit.edu/scratchr2/static/__
                                     61560e08176805f6f84b8d1dd737d59b__/
                                     pdfs/help/Getting-Started-Guide-Scratch2.pdf">
-                                    Getting Started Guide</a></h4>
+                                    Getting Started Guide</a>
+                                </h4>
                                     <p>This step-by-step guide (PDF) provides an easy introduction to Scratch.</p>
                                 </div>
                                  <div className="scratch-cards">
                                     <img src="/images/help/scratch-cards.png"></img>
-                                    <h4><a href="/help/cards">
-                                    Scratch Cards</a></h4>
+                                    <h4>
+                                        <a href="/help/cards">
+                                    Scratch Cards</a>
+                                </h4>
                                     <p>Each of these cards shows something you can do in Scratch.</p>
                                 </div>
                                 <div className="video-tutorials">
                                     <img src="/images/help/video-tutorials.png"></img>
-                                    <h4><a href="/help/videos">
-                                    Video Tutorials</a></h4>
-                                    <p>These videos include tips on using the paint editor, and 
+                                    <h4>
+                                        <a href="/help/videos">
+                                    Video Tutorials</a>
+                                </h4>
+                                    <p>These videos include tips on using the paint editor, and
                                     introduce how to program games and animations in Scratch</p>
                                 </div>
                             </div>
@@ -62,13 +68,15 @@ var View = React.createClass({
                     <Box title="Explore the Scratch Community">
                         <div className="box-contents">
                             <p><a href="/explore">Explore</a> projects and studios Scratchers are making</p>
-                            <p>Search for projects about: 
+                            <p>Search for projects about:
                                 <form class="search" action="/search/google_results/" method="get">
                                     <input type="text" name="q" />
                                 </form>
                             </p>
                             <p><a href="/discuss/6">Introduce</a> yourself in the Scratch forums</p>
-                            <p> Click <img src="/images/help/follow-button-small.png"></img> on any profile to receive updates</p>
+                            <p> Click
+                                <img src="/images/help/follow-button-small.png" />
+                                 on any profile to receive updates</p>
                         </div>
                     </Box>
                 </div>
@@ -93,23 +101,36 @@ var View = React.createClass({
                     </Box>
                     <Box title="Resources">
                         <div className="box-contents">
-                            <h4><a href="http://scratched.gse.harvard.edu/">Scratch Ed</a></h4>
-                            <p> An online community for educators using Scratch, with stories, discussions, and 
+                            <h4>
+                                <a href="http://scratched.gse.harvard.edu/">Scratch Ed</a>
+                            </h4>
+                            <p> An online community for educators using Scratch, with stories, discussions, and
                             resources, such as the Scratch curriculum guide.</p>
-                            <h4><a href="http://wiki.scratch.mit.edu/">Scratch Wiki</a></h4>
-                            <p> The Scratch Wiki contains a wide variety of articles by Scratchers for Scratchers, 
+                            <h4>
+                                <a href="http://wiki.scratch.mit.edu/">Scratch Wiki</a>
+                            </h4>
+                            <p> The Scratch Wiki contains a wide variety of articles by Scratchers for Scratchers,
                             including advanced topics and tutorials.</p>
-                            <h4><a href="/scratch2download">Scratch 2.0 Offline Editor</a></h4>
+                            <h4>
+                                <a href="/scratch2download">Scratch 2.0 Offline Editor</a>
+                            </h4>
                             <p> Download the Scratch 2 offline editor.</p>
-                            <h4><a href="/scratch_1.4">Scratch 1.4 Download</a></h4>
+                            <h4>
+                                <a href="/scratch_1.4">Scratch 1.4 Download</a>
+                            </h4>
                             <p> Links and information on the previous version of Scratch.</p>
-                            <h4><a href="http://wiki.scratch.mit.edu/wiki/How_to_connect_to_the_physical_world">
-                            Scratch and The Physical World</a></h4>
+                            <h4>
+                                <a href="http://wiki.scratch.mit.edu/wiki/How_to_connect_to_the_physical_world">
+                            Scratch and The Physical World</a>
+                        </h4>
                             <p> Connect your Scratch projects to the physical world with
                             <a href="http://www.makeymakey.com/"> MaKey MaKey</a>,
-                            <a href="https://education.lego.com/en-us/lesi/elementary/lego-education-wedo"> LEGO WeDo</a>, or
+                            <a href="https://education.lego.com/en-us/lesi/elementary/lego-education-wedo"> LEGO WeDo
+                            </a>, or
                             <a href="https://www.sparkfun.com/products/11888"> PicoBoard</a>.</p>
-                            <h4><a href="http://wiki.scratch.mit.edu/wiki/Scratch_Media">Scratch Logo</a></h4>
+                            <h4>
+                                <a href="http://wiki.scratch.mit.edu/wiki/Scratch_Media">Scratch Logo</a>
+                            </h4>
                             <p>Access the Scratch logo and other media files.</p>
                         </div>
                     </Box>
