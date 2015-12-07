ea4536f091e52eb8ac59ac00f0b8cd5f1d9fef7d
Ray Schamp <ray@scratch.mit.edu>
2015-12-07 17:43:04 -0500

    Make Jar formatting consistent with everything else

--- src/lib/jar.js
+++ src/lib/jar.js
@@ -1,41 +1,39 @@
 var cookie = require('cookie');
 var xhr = require('xhr');
 
-var Jar = {};
+var Jar = {
+    get: function (name, callback) {
+        // Get cookie by name
+        var obj = cookie.parse(document.cookie) || {};
 
-Jar.get = function (name, callback) {
-    // Get cookie by name
-    var obj = cookie.parse(document.cookie) || {};
+        // Handle optional callback
+        if (typeof callback === 'function') {
+            if (typeof obj === 'undefined') return callback('Cookie not found.');
+            return callback(null, obj[name]);
+        }
 
-    // Handle optional callback
-    if (typeof callback === 'function') {
-        if (typeof obj === 'undefined') return callback('Cookie not found.');
-        return callback(null, obj[name]);
-    }
+        return obj[name];
+    },
+    use: function (name, uri, callback) {
+        // Attempt to get cookie
+        Jar.get(name, function (err, obj) {
+            if (typeof obj !== 'undefined') return callback(null, obj);
 
-    return obj[name];
-};
-
-Jar.use = function (name, uri, callback) {
-    // Attempt to get cookie
-    Jar.get(name, function (err, obj) {
-        if (typeof obj !== 'undefined') return callback(null, obj);
-
-        // Make XHR request to cookie setter uri
-        xhr({
-            uri: uri
-        }, function (err) {
-            if (err) return callback(err);
-            Jar.get(name, callback);
+            // Make XHR request to cookie setter uri
+            xhr({
+                uri: uri
+            }, function (err) {
+                if (err) return callback(err);
+                Jar.get(name, callback);
+            });
         });
-    });
-};
-
-Jar.set = function (name, value) {
-    var obj = cookie.serialize(name, value);
-    var expires = '; expires=' + new Date(new Date().setYear(new Date().getFullYear() + 1)).toUTCString();
-    var path = '; path=/';
-    document.cookie = obj + expires + path;
+    },
+    set: function (name, value) {
+        var obj = cookie.serialize(name, value);
+        var expires = '; expires=' + new Date(new Date().setYear(new Date().getFullYear() + 1)).toUTCString();
+        var path = '; path=/';
+        document.cookie = obj + expires + path;
+    }
 };
 
 module.exports = Jar;
