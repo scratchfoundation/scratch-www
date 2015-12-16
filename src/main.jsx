8c374e096d3a97d692faac2291052d4231b102d3
Matthew Taylor <mewtaylor@gmail.com>
2015-12-16 10:43:37 -0500

    Move custom loader to `main.jsx`
    So that it's only done once.

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+var ReactIntl = require('react-intl');
+
+var customLanguages = require('../custom-locales.json');
 var render = require('./lib/render.jsx');
 
 require('./main.scss');
@@ -5,5 +8,8 @@ require('./main.scss');
 var Navigation = require('./components/navigation/navigation.jsx');
 var Footer = require('./components/footer/footer.jsx');
 
+for (var locale in customLanguages) {
+    ReactIntl.addLocaleData(customLanguages[locale]);
+}
 render(<Navigation />, document.getElementById('navigation'));
 render(<Footer />, document.getElementById('footer'));
