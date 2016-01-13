592f66c9a88e074098b170afdec10c7b7a3e2e0b
Matthew Taylor <mewtaylor@gmail.com>
2016-01-13 10:35:26 -0500

    Fix credits link
    Fixes #297

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -84,7 +84,9 @@ var About = React.createClass({
                                     <li><a href="/help"><FormattedMessage id='about.learnMoreHelp' /></a></li>
                                     <li><a href="/info/faq"><FormattedMessage id='about.learnMoreFaq' /></a></li>
                                     <li><a href="/parents"><FormattedMessage id='about.learnMoreParents' /></a></li>
-                                    <li><a href="/credits"><FormattedMessage id='about.learnMoreCredits' /></a></li>
+                                    <li>
+                                        <a href="/info/credits"><FormattedMessage id='about.learnMoreCredits' /></a>
+                                    </li>
                                 </ul>
                             </p>
                         </li>
