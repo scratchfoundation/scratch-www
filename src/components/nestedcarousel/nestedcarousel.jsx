c7cc1b626ffa616a68f4b0dd3ce397383b8a5e4a
morant <morant@media.mit.edu>
2016-02-19 15:11:23 -0500

    Revert "Revert "Fix review comments, and organize code a bit.""
    This reverts commit e9cb08d273bff4fcd67f3ae63aa12abd064abdc8.

--- src/components/nestedcarousel/nestedcarousel.jsx
+++ src/components/nestedcarousel/nestedcarousel.jsx
@@ -7,16 +7,24 @@ var Thumbnail = require('../thumbnail/thumbnail.jsx');
 
 require('slick-carousel/slick/slick.scss');
 require('slick-carousel/slick/slick-theme.scss');
-require('./tipsslider.scss');
+require('./nestedcarousel.scss');
 
-var TipsSlider = React.createClass({
-    type: 'TipsSlider',
+
+{/*
+    NestedCarousel is used to show a carousel, where each slide is composed of a few
+    thumbnails (for example, to show step-by-syep tips, where each stage has a few steps).
+    It creates the thumbnails without links.
+
+    Each slide has a title, and then a list of thumbnails, that will be shown together.
+*/}
+var NestedCarousel = React.createClass({
+    type: 'NestedCarousel',
     propTypes: {
         items: React.PropTypes.array
     },
     getDefaultProps: function () {
         return {
-            items: require('./tipsslider.json'),
+            items: require('./nestedcarousel.json')
         };
     },
     render: function () {
@@ -33,32 +41,35 @@ var TipsSlider = React.createClass({
         var arrows = this.props.items.length > settings.slidesToShow;
         
         var classes = classNames(
-            'tipsslider',
+            'nestedcarousel',
+            'carousel',
             this.props.className
         );
         
         var stages = [];
         for (var i=0; i < this.props.items.length; i++) {
-            var items = this.props.items[i].tips;
+            var items = this.props.items[i].thumbnails;
             var thumbnails = [];
             for (var j=0; j < items.length; j++) {
-                    var item = items[j];
-                    thumbnails.push(<Thumbnail key={item.id}
+                var item = items[j];
+                thumbnails.push(
+                    <Thumbnail key={'inner_' + i + '_' + j}
                                title={item.title}
-                               src={item.thumbnailUrl} />)
+                               src={item.thumbnailUrl}
+                               linkTitle = {false} />);
             }
             stages.push(
-            <div className="testing" key={"stage_" + i}>
+            <div key={'outer_' + i}>
                 <h3>{this.props.items[i].title}</h3>
                 {thumbnails}
-            </div>)
-        } 
+            </div>);
+        }
         return (
             <Slider className={classes} arrows={arrows} {... settings}>
-                {stages}    
+                {stages}
             </Slider>
         );
     }
 });
 
-module.exports = TipsSlider;
+module.exports = NestedCarousel;
