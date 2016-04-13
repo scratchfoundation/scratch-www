5205e4d2d8616ed4ca543b39ab7cb73aa9f0f73e
The_Grits <gritsprogramming@gmail.com>
2016-04-13 09:42:50 -0400

    why hello lint

--- src/views/guidelines/guidelines.jsx
+++ src/views/guidelines/guidelines.jsx
@@ -6,21 +6,32 @@ require('./guidelines.scss');
 
 var Guidelines = React.createClass({
     type: 'Guidelines',
-    render: function() {
+    render: function () {
         return (
             <div className="body">
                 <div className="top">
                     <h1>Scratch Community Guidelines</h1>
                 </div>
                 <div className="bottom">
-                    <p>We need everyone's help to keep Scratch a friendly and creative community where people with different backgrounds and interests feel welcome.</p>
+                    <p>We need everyone's help to keep Scratch a friendly and creative 
+                    community where people with different backgrounds and interests feel welcome.</p>
                     <ul>
-                        <li><b>Be respectful.</b> When sharing projects or posting comments, remember that people of many different ages and backgrounds will see what you've shared.</li>
-                        <li><b>Be constructive.</b> When commenting on others' projects, say something you like about it and offer suggestions.</li>
-                        <li><b>Share.</b> You are free to remix projects, ideas, images, or anything else you find on Scratch - and anyone can use anything that you share. Be sure to give credit when you remix.</li>
-                        <li><b>Keep personal info private.</b> For safety reasons, don't use real names or post contact info like phone numbers or addresses.</li>
-                        <li><b>Be honest.</b> Don't try to impersonate other Scratchers, spread rumors, or otherwise try to trick the community.</li>
-                        <li><b>Help keep the site friendly.</b> If you think a project or comment is mean, insulting, too violent, or otherwise inappropriate, click "Report" to let us know about it.</li>
+                        <li><b>Be respectful.</b><p> When sharing projects or posting 
+                        comments, remember that people of many different ages and 
+                        backgrounds will see what you've shared.</p></li>
+                        <li><b>Be constructive.</b><p> When commenting on others' projects, 
+                        say something you like about it and offer suggestions.</p></li>
+                        <li><b>Share.</b><p> You are free to remix projects, ideas, images, 
+                        or anything else you find on Scratch - and anyone can use anything 
+                        that you share. Be sure to give credit when you remix.</p></li>
+                        <li><b>Keep personal info private.</b><p> For safety reasons, 
+                        don't use real names or post contact info like phone numbers or 
+                        addresses.</p></li>
+                        <li><b>Be honest.</b><p> Don't try to impersonate other Scratchers, 
+                        spread rumors, or otherwise try to trick the community.</p></li>
+                        <li><b>Help keep the site friendly.</b><p> If you think a project or 
+                        comment is mean, insulting, too violent, or otherwise inappropriate, 
+                        click "Report" to let us know about it.</p></li>
                     </ul>
                     <p>Scratch welcomes people of all ages, races, ethnicities, religions, sexual orientations, and gender identities.</p>
                     <img src="//cdn.scratch.mit.edu/scratchr2/static/__59036f4a23f9f535f48b54afa64f0d97__//images/help/spritesforcommunityguid.png" alt="lineosprites"/>
