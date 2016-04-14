31cbd1b735be42104390507a052b3f2f2b9fee21
The_Grits <gritsprogramming@gmail.com>
2016-04-14 14:06:29 -0400

    Hopefully squashed? (#2)
    * FormattedMessage
    
    Awesomesauce, it even looks more organized!
    
    * Update guidelines.jsx
    
    * Classes not ids
    
    * Box!
    
    Hopefully I did it correctly...
    
    * top is nonexistant
    
    Since I used the Box title

--- src/views/guidelines/guidelines.jsx
+++ src/views/guidelines/guidelines.jsx
@@ -1,6 +1,9 @@
 var React = require('react');
+var FormattedMessage = require('react-intl').FormattedMessage;
+
 var render = require('../../lib/render.jsx');
 var Page = require('../../components/page/page.jsx');
+var Box = require('../../components/box/box.jsx');
 
 require('./guidelines.scss');
 
@@ -8,36 +11,39 @@ var Guidelines = React.createClass({
     type: 'Guidelines',
     render: function () {
         return (
-            <div className="body">
-                <div className="top">
-                    <h1>Scratch Community Guidelines</h1>
-                </div>
+            <Box title={<FormattedMessage id='guidelines.title' />}>
                 <div className="bottom">
-                    <p>We need everyone's help to keep Scratch a friendly and creative
-                     community where people with different backgrounds and interests feel welcome.</p>
+                    <p><FormattedMessage id='guidelines.header' /></p>
                     <ul>
-                        <li><b>Be respectful.</b><p> When sharing projects or posting
-                         comments, remember that people of many different ages and
-                         backgrounds will see what you've shared.</p></li>
-                        <li><b>Be constructive.</b><p> When commenting on others' projects,
-                         say something you like about it and offer suggestions.</p></li>
-                        <li><b>Share.</b><p> You are free to remix projects, ideas, images,
-                         or anything else you find on Scratch - and anyone can use anything
-                         that you share. Be sure to give credit when you remix.</p></li>
-                        <li><b>Keep personal info private.</b><p> For safety reasons,
-                         don't use real names or post contact info like phone numbers or
-                         addresses.</p></li>
-                        <li><b>Be honest.</b><p> Don't try to impersonate other Scratchers,
-                         spread rumors, or otherwise try to trick the community.</p></li>
-                        <li><b>Help keep the site friendly.</b><p> If you think a project or
-                         comment is mean, insulting, too violent, or otherwise inappropriate,
-                         click "Report" to let us know about it.</p></li>
+                        <li>
+                            <b><FormattedMessage id='guidelines.respectheader' /></b>
+                            <p><FormattedMessage id='guidelines.respectbody' /></p>
+                        </li>
+                        <li>
+                            <b><FormattedMessage id='guidelines.constructiveheader' /></b>
+                            <p><FormattedMessage id='guidelines.constructivebody' /></p>
+                        </li>
+                        <li>
+                            <b><FormattedMessage id='guidelines.shareheader' /></b>
+                            <p><FormattedMessage id='guidelines.sharebody' /></p>
+                        </li>
+                        <li>
+                            <b><FormattedMessage id='guidelines.privacyheader' /></b>
+                            <p><FormattedMessage id='guidelines.privacybody' /></p>
+                        </li>
+                        <li>
+                            <b><FormattedMessage id='guidelines.honestyheader' /></b>
+                            <p><FormattedMessage id='guidelines.honestybody' /></p>
+                        </li>
+                        <li>
+                            <b><FormattedMessage id='guidelines.friendlyheader' /></b>
+                            <p><FormattedMessage id='guidelines.friendlybody' /></p>
+                        </li>
                     </ul>
-                    <p>Scratch welcomes people of all ages, races, ethnicities, religions,
-                     sexual orientations, and gender identities.</p>
+                    <p><FormattedMessage id='guidelines.footer' /></p>
                     <img src="//cdn.scratch.mit.edu/scratchr2/static/images/help/spritesforcommunityguid.png" alt="sprites"/>
                 </div>
-            </div>
+            </Box>
         );
     }
 });
