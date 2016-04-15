8ed711a148e4421a4c72ac35fa4a50d1288e128d
The_Grits <gritsprogramming@gmail.com>
2016-04-15 08:26:24 -0400

    indent lint error fix

--- src/views/dmca/dmca.jsx
+++ src/views/dmca/dmca.jsx
@@ -7,29 +7,29 @@ var Box = require('../../components/box/box.jsx');
 require('./dmca.scss');
 
 var Dmca = React.createClass({
-  type: 'Dmca',
-  render: function () {
-    return (
-      <Box title={'DMCA'}>
-        <p><FormattedMessage id='dmca.intro' /></p>
-        <br/>
-        <p>Copyright Agent / Mitchel Resnick</p>
-        <p>MIT Media Laboratory</p>
-        <p>77 Massachusetts Ave</p>
-        <p>Room E14-445A</p>
-        <p>Cambridge, MA 02139</p>
-        <p>Tel: (617) 253-9783</p>
-        <br/>
-        <p><FormattedMessage id='dmca.llkresponse' /></p>
-        <br/>
-        <p><FormattedMessage id='dmca.assessment' /></p>
-        <br/>
-        <p><FormattedMessage id='dmca.eyetoeye' /></p>
-        <br/>
-        <p><FormattedMessage id='dmca.afterfiling' /></p>
-      </Box>
-    );
-  }
+    type: 'Dmca',
+    render: function () {
+      return (
+        <Box title={'DMCA'}>
+          <p><FormattedMessage id='dmca.intro' /></p>
+          <br/>
+          <p>Copyright Agent / Mitchel Resnick</p>
+          <p>MIT Media Laboratory</p>
+          <p>77 Massachusetts Ave</p>
+          <p>Room E14-445A</p>
+          <p>Cambridge, MA 02139</p>
+          <p>Tel: (617) 253-9783</p>
+          <br/>
+          <p><FormattedMessage id='dmca.llkresponse' /></p>
+          <br/>
+          <p><FormattedMessage id='dmca.assessment' /></p>
+          <br/>
+          <p><FormattedMessage id='dmca.eyetoeye' /></p>
+          <br/>
+          <p><FormattedMessage id='dmca.afterfiling' /></p>
+        </Box>
+      );
+    }
 });
 
 render(<Page><Dmca /></Page>, document.getElementById('app'));
