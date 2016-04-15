69c6cb95f2c2dabf175599762e8e3614891eb717
The_Grits <gritsprogramming@gmail.com>
2016-04-15 08:31:10 -0400

    lint again

--- src/views/dmca/dmca.jsx
+++ src/views/dmca/dmca.jsx
@@ -9,26 +9,26 @@ require('./dmca.scss');
 var Dmca = React.createClass({
     type: 'Dmca',
     render: function () {
-      return (
-        <Box title={'DMCA'}>
-          <p><FormattedMessage id='dmca.intro' /></p>
-          <br/>
-          <p>Copyright Agent / Mitchel Resnick</p>
-          <p>MIT Media Laboratory</p>
-          <p>77 Massachusetts Ave</p>
-          <p>Room E14-445A</p>
-          <p>Cambridge, MA 02139</p>
-          <p>Tel: (617) 253-9783</p>
-          <br/>
-          <p><FormattedMessage id='dmca.llkresponse' /></p>
-          <br/>
-          <p><FormattedMessage id='dmca.assessment' /></p>
-          <br/>
-          <p><FormattedMessage id='dmca.eyetoeye' /></p>
-          <br/>
-          <p><FormattedMessage id='dmca.afterfiling' /></p>
-        </Box>
-      );
+        return (
+            <Box title={'DMCA'}>
+                <p><FormattedMessage id='dmca.intro' /></p>
+                <br/>
+                <p>Copyright Agent / Mitchel Resnick</p>
+                <p>MIT Media Laboratory</p>
+                <p>77 Massachusetts Ave</p>
+                <p>Room E14-445A</p>
+                <p>Cambridge, MA 02139</p>
+                <p>Tel: (617) 253-9783</p>
+                <br/>
+                <p><FormattedMessage id='dmca.llkresponse' /></p>
+                <br/>
+                <p><FormattedMessage id='dmca.assessment' /></p>
+                <br/>
+                <p><FormattedMessage id='dmca.eyetoeye' /></p>
+                <br/>
+                <p><FormattedMessage id='dmca.afterfiling' /></p>
+            </Box>
+          );
     }
 });
 
