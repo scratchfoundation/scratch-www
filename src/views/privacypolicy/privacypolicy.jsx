abe63bea228753e03900aea19e9c8af0e3f72ee7
The_Grits <gritsprogramming@gmail.com>
2016-05-10 23:22:27 -0400

    whoops
    lol the bracket types were switched around

--- src/views/privacypolicy/privacypolicy.jsx
+++ src/views/privacypolicy/privacypolicy.jsx
@@ -11,58 +11,60 @@ var Privacypolicy = React.createClass({
     type: 'Privacypolicy',
     render: function () {
         return (
-            <Box title={<FormattedMessage id='privacypolicy.title' />}>
-                <p><FormattedHTMLMessage id='privacypolicy.introOne' /></p>
-                <p><i><FormattedMessage id='privacypolicy.introTwo' /></i></p>
-                <h3><FormattedMessage id='privacypolicy.whatinfo' /></h3>
-                <p>
-                    <b><FormattedMessage id='privacypolicy.accountinfotitle' /></b>
-                    <FormattedMessage id='privacypolicy.accountinfobody' />
-                </p>
-                <p>
-                    <b><FormattedMessage id='privacypolicy.usergeneratedtitle' /></b>
-                    <FormattedHTMLMessage id='privacypolicy.usergeneratedbody' />
-                </p>
-                <p>
-                    <b><FormattedMessage id='privacypolicy.usageinfotitle' /></b>
-                    <FormattedMessage id='privacypolicy.usageinfobody' />
-                </p>
-                <p>
-                    <b><FormattedMessage id='privacypolicy.googleanalyticstitle' /></b>
-                    <FormattedMessage id='privacypolicy.googleanalyticsbody' />
-                </p>
-                <p>
-                    <b><FormattedMessage id='privacypolicy.cookiestitle' /></b>
-                    <FormattedMessage id='privacypolicy.cookiesbody' />
-                </p>
-                <h3><FormattedMessage id='privacypolicy.infousage' /></h3>
-                <ul>
-                    <li><FormattedMessage id='privacypolicy.agegender' /></li>
-                    <li><FormattedMessage id='privacypolicy.birthmonth' /></li>
-                    <li><FormattedMessage id='privacypolicy.emailaddress' /></li>
-                    <li><FormattedMessage id='privacypolicy.emailupdates' /></li>
-                    <li><FormattedHTMLMessage id='privacypolicy.additionalemails' /></li>
-                    <li><FormattedMessage id='privacypolicy.abusivebehavior' /></li>
-                    <li><FormattedHTMLMessage id='privacypolicy.researchinfo' /></li>
-                    <li><FormattedMessage id='privacypolicy.thirdparty' /></li>
-                    <li>
-                        <FormattedMessage id='privacypolicy.exceptions' />
-                        <ul>
-                            <li><FormattedMessage id='privacypolicy.lawrequirement' /></li>
-                            <li><FormattedMessage id='privacypolicy.technicalreasons' /></li>
-                        </ul>
-                    </li>
-                </ul>
-                <h3><FormattedMessage id='privacypolicy.updateheader' /></h3>
-                <p><FormattedHTMLMessage id='privacypolicy.updateuser' /></p>
-                <p><FormattedMessage id='privacypolicy.deleteuser' /></p>
-                <h3><FormattedMessage id='privacypolicy.infoprotectionheader' /></h3>
-                <p><FormattedMessage id='privacypolicy.infoprotectionbody' /></p>
-                <h3><FormattedMessage id='privacypolicy.policychangesheader' /></h3>
-                <p><FormattedMessage id='privacypolicy.policychangesbody' /></p>
-            </Box>
+            <div className="inner">
+                <Box title={<FormattedMessage id='privacypolicy.title' />}>
+                    <p><FormattedHTMLMessage id='privacypolicy.introOne' /></p>
+                    <p><i><FormattedMessage id='privacypolicy.introTwo' /></i></p>
+                    <h3><FormattedMessage id='privacypolicy.whatinfo' /></h3>
+                    <p>
+                        <b><FormattedMessage id='privacypolicy.accountinfotitle' /></b>
+                        <FormattedMessage id='privacypolicy.accountinfobody' />
+                    </p>
+                    <p>
+                        <b><FormattedMessage id='privacypolicy.usergeneratedtitle' /></b>
+                        <FormattedHTMLMessage id='privacypolicy.usergeneratedbody' />
+                    </p>
+                    <p>
+                        <b><FormattedMessage id='privacypolicy.usageinfotitle' /></b>
+                        <FormattedMessage id='privacypolicy.usageinfobody' />
+                    </p>
+                    <p>
+                        <b><FormattedMessage id='privacypolicy.googleanalyticstitle' /></b>
+                        <FormattedMessage id='privacypolicy.googleanalyticsbody' />
+                    </p>
+                    <p>
+                        <b><FormattedMessage id='privacypolicy.cookiestitle' /></b>
+                        <FormattedMessage id='privacypolicy.cookiesbody' />
+                    </p>
+                    <h3><FormattedMessage id='privacypolicy.infousage' /></h3>
+                    <ul>
+                        <li><FormattedMessage id='privacypolicy.agegender' /></li>
+                        <li><FormattedMessage id='privacypolicy.birthmonth' /></li>
+                        <li><FormattedMessage id='privacypolicy.emailaddress' /></li>
+                        <li><FormattedMessage id='privacypolicy.emailupdates' /></li>
+                        <li><FormattedHTMLMessage id='privacypolicy.additionalemails' /></li>
+                        <li><FormattedMessage id='privacypolicy.abusivebehavior' /></li>
+                        <li><FormattedHTMLMessage id='privacypolicy.researchinfo' /></li>
+                        <li><FormattedMessage id='privacypolicy.thirdparty' /></li>
+                        <li>
+                            <FormattedMessage id='privacypolicy.exceptions' />
+                            <ul>
+                                <li><FormattedMessage id='privacypolicy.lawrequirement' /></li>
+                                <li><FormattedMessage id='privacypolicy.technicalreasons' /></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <h3><FormattedMessage id='privacypolicy.updateheader' /></h3>
+                    <p><FormattedHTMLMessage id='privacypolicy.updateuser' /></p>
+                    <p><FormattedMessage id='privacypolicy.deleteuser' /></p>
+                    <h3><FormattedMessage id='privacypolicy.infoprotectionheader' /></h3>
+                    <p><FormattedMessage id='privacypolicy.infoprotectionbody' /></p>
+                    <h3><FormattedMessage id='privacypolicy.policychangesheader' /></h3>
+                    <p><FormattedMessage id='privacypolicy.policychangesbody' /></p>
+                </Box>
+            </div>
         );
     }
-)};
+});
 
 render(<Page><Privacypolicy /></Page>, document.getElementById('app'));
