b65332c5d433f92a2c0c6ebff7f41635cfa61435
Ray Schamp <ray@scratch.mit.edu>
2016-05-17 15:57:23 -0400

    Set up Raven within init.js
    Now that Raven is bundled with webpack, it's not available in the global context, so require it and configure it within the init module.
    
    I struggled to figure out how to expose the raven-js module as a global, as all the combinations of `externals` and `ProvidePlugin` only applied to webpacked modules, not the global window.  The correct way to do it seems to be exports-loader but that looked rather hacky and hard to understand.

--- webpack.config.js
+++ webpack.config.js
@@ -110,7 +110,8 @@ module.exports = {
             }
         }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"' + process.env.NODE_ENV + '"'
+            'process.env.NODE_ENV': '"' + (process.env.NODE_ENV || 'development') + '"',
+            'process.env.SENTRY_DSN': '"' + (process.env.SENTRY_DSN || '') + '"'
         }),
         new webpack.optimize.CommonsChunkPlugin('common', 'js/common.bundle.js'),
         new webpack.optimize.OccurenceOrderPlugin()
