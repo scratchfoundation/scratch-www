9bd63bd11aede31d6316552d1afec45b81e00367
The_Grits <gritsprogramming@gmail.com>
2016-05-26 11:23:17 -0400

    Spacing

--- src/views/termsofuse/termsofuse.jsx
+++ src/views/termsofuse/termsofuse.jsx
@@ -37,7 +37,7 @@ var Termsofuse = React.createClass({
                     <p>
                         1.3 Scratch is open to children and adults of all ages, and
                          we ask that you keep this in mind when using the Scratch
-                         services. When you use Scratch, you agree to abide by the
+                         services. When you use Scratch, you agree to abide by the{' '}
                          <a href="/community_guidelines">Scratch Community Guidelines</a>.
                     </p>
                     <p>
