4f75713bd56c323a6364933d64989266d59e1af4
The_Grits <gritsprogramming@gmail.com>
2016-05-27 09:56:56 -0400

    Update privacypolicy.jsx

--- src/views/privacypolicy/privacypolicy.jsx
+++ src/views/privacypolicy/privacypolicy.jsx
@@ -152,7 +152,7 @@ var Privacypolicy = React.createClass({
                         You can update your password, email address, and country through
                          the <a href="/account/password_change">Account Settings</a> page.
                          You can also reset your password through the{' '}
-                          <a href="/account/password_reset">Account Reset</a>{' '}
+                          <a href="/account/password_reset">Password Reset</a>{' '}
                          page. You cannot change your username, but you can make a new
                          account and manually copy your projects to the new account.
                     </p>
