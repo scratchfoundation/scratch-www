25402113bf1f8e794065c7e58bb46228d6fdee13
Technoboy10 <technoboy10@fake.email>
2016-07-27 09:26:33 -0400

    tweak a few things before WIP PR

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,7 +5,6 @@ var detailsReducer = require('./conference-details.js').detailsReducer;
 var permissionsReducer = require('./permissions.js').permissionsReducer;
 var sessionReducer = require('./session.js').sessionReducer;
 var tokenReducer = require('./token.js').tokenReducer;
-// var layout = require('./layout.js');
 
 var appReducer = combineReducers({
     session: sessionReducer,
