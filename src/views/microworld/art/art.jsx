2af34efa98e74a799e01fd898460c7b6ba080c90
morant <morant@media.mit.edu>
2016-08-26 15:51:42 -0400

    Fix some failed tests.

--- src/views/microworld/art/art.jsx
+++ src/views/microworld/art/art.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var render = require('../../../lib/render.jsx');
 var Microworld = require('../../../components/microworld/microworld.jsx');
-var Page = require('../../../components/page/www/page.jsx')
+var Page = require('../../../components/page/www/page.jsx');
 
 var microworldData = require('./art.json');
 
