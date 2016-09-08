a497fed3389d067f9a02d5dedece3b97002e1f83
Ray Schamp <ray@scratch.mit.edu>
2016-09-08 12:48:07 -0400

    Remove token storage
    We're not using it yet, and it won't be coming from the cookie when we do.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,11 +4,9 @@ var scheduleReducer = require('./conference-schedule.js').scheduleReducer;
 var detailsReducer = require('./conference-details.js').detailsReducer;
 var permissionsReducer = require('./permissions.js').permissionsReducer;
 var sessionReducer = require('./session.js').sessionReducer;
-var tokenReducer = require('./token.js').tokenReducer;
 
 var appReducer = combineReducers({
     session: sessionReducer,
-    token: tokenReducer,
     permissions: permissionsReducer,
     conferenceSchedule: scheduleReducer,
     conferenceDetails: detailsReducer
--- src/redux/session.js
+++ src/redux/session.js
@@ -3,7 +3,6 @@ var defaults = require('lodash.defaults');
 
 var api = require('../lib/api');
 var permissionsActions = require('./permissions.js');
-var tokenActions = require('./token.js');
 
 var Types = keyMirror({
     SET_SESSION: null,
@@ -86,7 +85,6 @@ module.exports.refreshSession = function () {
                     window.location.pathname !== '/classes/student_password_reset/') {
                 return window.location = '/classes/student_password_reset/';
             } else {
-                dispatch(tokenActions.getToken());
                 dispatch(module.exports.setSession(body));
                 dispatch(module.exports.setStatus(module.exports.Status.FETCHED));
 
