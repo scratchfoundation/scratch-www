e06cf2b134e8632b7658da1bedda1b08ad89d267
jwzimmer <jwzimmer@media.mit.edu>
2016-09-21 10:41:24 -0400

    simplify checking that the href is at the end of the url with ray's suggestion

--- test/integration/check_navbar_links.js
+++ test/integration/check_navbar_links.js
@@ -12,18 +12,6 @@ var driver = new seleniumWebdriver.Builder().withCapabilities(seleniumWebdriver.
 //open scratch.ly in a new instance of the browser
 driver.get('https://scratch.ly');
 
-//return only the part of the URL that is in the href in the page's html,
-//and the index that the href was found at
-function getHrefFromUrl (url, expectedHref) {
-    var hrefOnly = '';
-    var hrefIndex = url.lastIndexOf(expectedHref);
-    if (hrefIndex != -1) {
-        hrefOnly = url.substr(hrefIndex);
-    }
-    return {hrefIndex: hrefIndex,
-            hrefOnly: hrefOnly};
-}
-
 //find the create link within the navbar
 //the create link depends on whether the user is signed in or not (tips window opens)
 tap.test('checkCreateLinkWhenSignedOut', function (t) {
@@ -32,15 +20,10 @@ tap.test('checkCreateLinkWhenSignedOut', function (t) {
     createLinkSignedOut.getAttribute('href').then( function (url) {
         //expected value of the href
         var expectedHref = '/projects/editor/?tip_bar=home';
-        var hrefInfo = getHrefFromUrl(url, expectedHref);
-        var hrefIndex = hrefInfo.hrefIndex;
-        var hrefOnly = hrefInfo.hrefOnly;
         //the create href should match `/projects/editor/?tip_bar=home`
-        t.equal(expectedHref, hrefOnly);
         //the create href should be at the end of the URL
-        var urlLength = url.length;
-        var urlLengthFromHrefInfo = hrefOnly.length + hrefIndex;
-        t.equal(urlLengthFromHrefInfo, urlLength);
+        t.equal(url.substr(-expectedHref.length), expectedHref);
+        console.log(url.substr(-expectedHref.length));
         t.end();
     });
 });
