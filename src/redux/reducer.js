f77b22b07b19e7677e8dae23f58f3fb5c7bba52e
The_Grits <gritsprogramming@gmail.com>
2016-10-20 21:30:32 -0400

    Update with Reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,12 +4,14 @@ var scheduleReducer = require('./conference-schedule.js').scheduleReducer;
 var detailsReducer = require('./conference-details.js').detailsReducer;
 var permissionsReducer = require('./permissions.js').permissionsReducer;
 var sessionReducer = require('./session.js').sessionReducer;
+var navigation = require('./navigation.js').navigation;
 
 var appReducer = combineReducers({
     session: sessionReducer,
     permissions: permissionsReducer,
     conferenceSchedule: scheduleReducer,
-    conferenceDetails: detailsReducer
+    conferenceDetails: detailsReducer,
+    navigation: navigationReducer
 });
 
 module.exports = appReducer;
