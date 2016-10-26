379ac7855140940aceee4a1e812251dd471549fe
The_Grits <gritsprogramming@gmail.com>
2016-10-26 10:20:16 -0400

    Navigation Redux

--- src/redux/navigation.js
+++ src/redux/navigation.js
@@ -1,10 +1,24 @@
+var keyMirror = require('keymirror');
+
+var Types = keyMirror({
+    SET_SEARCH_TERM: null
+});
+
 module.exports.navigationReducer = function (state, action) {
     if(typeof state = 'undefined') {
         state = '';
     }
-    return state;
+    switch (action.type) {
+        case Types.SET_SEARCH_TERM:
+            return action.setSearchTerm;
+        default:
+            return state;
+    }
 };
 
-module.exports.setSearchTerm = function () {
-    return this.props.searchTerm;
+module.exports.setSearchTerm = function (searchTerm) {
+    return {
+        type: Types.SET_SEARCH_TERM,
+        searchTerm: searchTerm
+    };
 };
