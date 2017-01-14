4eb3287c0e388bb413dcfdb409d75c6afd06daf6
The_Grits <TheGrits@users.noreply.github.com>
2017-01-13 23:46:26 -0500

    FormattedMessage and FormattedHTMLMessage

--- src/views/credits/credits.jsx
+++ src/views/credits/credits.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var render = require('../../lib/render.jsx');
+var FormattedHTMLMessage = require('react-intl').FormattedHTMLMessage;
+var FormattedMessage = require('react-intl').FormattedMessage;
 
 var Page = require('../../components/page/www/page.jsx');
 
