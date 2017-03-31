0f87a65622b44636f237eeda9117cb9598f460e9
The_Grits <TheGrits@users.noreply.github.com>
2017-03-31 19:35:33 -0400

    importy

---
+++ src/views/download/download.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var render = require('../../../lib/render.jsx');
+
+var FormattedHTMLMessage = require('react-intl').FormattedHTMLMessage;
+var FormattedMessage = require('react-intl').FormattedMessage;
+
+var Page = require('../../../components/page/www/page.jsx');
+var InformationPage = require('../../../components/informationpage/informationpage.jsx');
