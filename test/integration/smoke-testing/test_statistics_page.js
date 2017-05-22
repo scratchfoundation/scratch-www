c27fb7f7c1c0e1cbb74f514742bcac56217af7ae
jwzimmer <jwzimmer@media.mit.edu>
2017-05-22 11:36:12 -0400

    added additional tests for more charts on the stats page

--- test/integration/smoke-testing/test_statistics_page.js
+++ test/integration/smoke-testing/test_statistics_page.js
@@ -2,7 +2,7 @@ require('chromedriver');
 var tap = require('tap');
 var seleniumWebdriver = require('selenium-webdriver');
 
-var rootUrl = process.env.ROOT_URL || 'https://scratch.ly';
+var rootUrl = process.env.ROOT_URL || 'https://scratch.mit.edu';
 
 //chrome driver
 var driver = new seleniumWebdriver.Builder().withCapabilities(seleniumWebdriver.Capabilities.chrome()).build();
@@ -10,7 +10,7 @@ var driver = new seleniumWebdriver.Builder().withCapabilities(seleniumWebdriver.
 seleniumWebdriver.SELENIUM_PROMISE_MANAGER=0;
 
 //number of tests in the plan
-tap.plan(1);
+tap.plan(3);
 
 tap.tearDown(function () {
     //quit the instance of the browser
@@ -22,9 +22,9 @@ tap.beforeEach(function () {
     return driver.get(rootUrl + '/statistics');
 });
 
-// to verify the Activity Chart's presence, checks the title,
-// a label on the X axis, and a label on the Y axis
-tap.test('activityChartShouldExist', function (t) {
+// to verify the Monthly Activity Trends Chart's presence, checks the title,
+// a label on the X axis, and a label in the legend
+tap.test('activityTrendsChartShouldExist', function (t) {
     var chartTitle = 'Monthly Activity Trends';
     var chartLegendLabel = 'New Projects';
     var chartXLabel = '01/2008';
@@ -42,6 +42,76 @@ tap.test('activityChartShouldExist', function (t) {
                     t.equal(text, chartLegendLabel);})
                 .then( function () {
                     driver.findElement(seleniumWebdriver.By.xpath('//div[@id="activity_chart"]/*[name()="svg"]' +
+                    '/*[name()="g"]/*[name()="g"]/*[name()="g"]/*[name()="g"]/*[name()="g"]' +
+                    '/*[name()="g"]'))
+                        .then( function (element) {
+                            return element.getText('text');})
+                        .then( function (text) {
+                            t.equal(text, chartXLabel);
+                            t.end();
+                        });
+                });
+        });
+});
+
+// to verify the Active Users Chart's presence, checks the title,
+// a label on the X axis, and a label in the legend
+tap.test('activeUsersChartShouldExist', function (t) {
+    var chartTitle = 'Monthly Active Users';
+    var chartLegendLabel = 'Project Creators';
+    var chartXLabel = '01/2008';
+    driver.findElements(seleniumWebdriver.By
+        .css('div.box-head h3'))
+        .then( function (elements) {
+            var element = elements[1];
+            return element.getText('h3');})
+        .then( function (text) {
+            t.equal(text, chartTitle);})
+        .then( function () {
+            driver.findElements(seleniumWebdriver.By.css('g.nv-series'))
+                .then( function (elements) {
+                    // the 3 labels in the first chart's legend are elements 0 - 2 in this list
+                    var element = elements[3];
+                    return element.getText('text');})
+                .then( function (text) {
+                    t.equal(text, chartLegendLabel);})
+                .then( function () {
+                    driver.findElement(seleniumWebdriver.By.xpath('//div[@id="active_user_chart"]/*[name()="svg"]' +
+                    '/*[name()="g"]/*[name()="g"]/*[name()="g"]/*[name()="g"]/*[name()="g"]' +
+                    '/*[name()="g"]'))
+                        .then( function (element) {
+                            return element.getText('text');})
+                        .then( function (text) {
+                            t.equal(text, chartXLabel);
+                            t.end();
+                        });
+                });
+        });
+});
+
+// to verify the Comment Activity Chart's presence, checks the title,
+// a label on the X axis, and a label in the legend
+tap.test('commentActivityChartShouldExist', function (t) {
+    var chartTitle = 'Monthly Comment Activity';
+    var chartLegendLabel = 'Stacked';
+    var chartXLabel = '01/2008';
+    driver.findElements(seleniumWebdriver.By
+        .css('div.box-head h3'))
+        .then( function (elements) {
+            var element = elements[5];
+            return element.getText('h3');})
+        .then( function (text) {
+            t.equal(text, chartTitle);})
+        .then( function () {
+            driver.findElements(seleniumWebdriver.By.css('g.nv-series'))
+                .then( function (elements) {
+                    // the labels from previous charts are elements 0 - 6 in this list
+                    var element = elements[7];
+                    return element.getText('text');})
+                .then( function (text) {
+                    t.equal(text, chartLegendLabel);})
+                .then( function () {
+                    driver.findElement(seleniumWebdriver.By.xpath('//div[@id="comment_chart"]/*[name()="svg"]' +
                     '/*[name()="g"]/*[name()="g"]/*[name()="g"]/*[name()="g"]/*[name()="g"]' +
                     '/*[name()="g"]'))
                         .then( function (element) {
