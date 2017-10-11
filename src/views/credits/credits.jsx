4ac00de96158b959be5a6facd745ac775f499e62
Sheshank Shankar <sheshank@outlook.com>
2017-10-11 13:25:26 -0700

    Update credits.jsx

--- src/views/credits/credits.jsx
+++ src/views/credits/credits.jsx
@@ -64,8 +64,7 @@ var Credits = React.createClass({
 
                     <li>
                         <img src="//cdn.scratch.mit.edu/get_image/user/246290_170x170.png" alt="Sarah Avatar" />
-                        <span className="name">Sarah Otts</span>
-                    </li>
+                        <span className="name">Sarah Otts</span>                    </li>
 
                     <li>
                         <img src="//cdn.scratch.mit.edu/get_image/user/2286560_170x170.png" alt="Carmelo Avatar" />
@@ -113,7 +112,6 @@ var Credits = React.createClass({
                     </li>
                     <li>
                         <img src="//cdn.scratch.mit.edu/get_image/user/4373707_170x170.png" alt="Matthew Avatar" />
-
                         <span className="name">Matthew Taylor</span>
                     </li>
 
@@ -121,8 +119,6 @@ var Credits = React.createClass({
                         <img src="//cdn.scratch.mit.edu/get_image/user/2678986_170x170.png" alt="Moran Avatar" />
                         <span className="name">Moran Tsur</span>
                     </li>
-
-                    
 
                     <li>
                         <img src="//cdn.scratch.mit.edu/get_image/user/3532363_170x170.png" alt="Chris Avatar" />
@@ -167,16 +163,6 @@ var Credits = React.createClass({
                     <li>
                         <img src="//cdn2.scratch.mit.edu/get_image/user/5311910_170x170.png" alt="Carolina Avatar" />
                         <span className="name">Carolina Kaufman</span>
-                    </li>
-
-                    <li>
-                        <img src="//cdn.scratch.mit.edu/get_image/user/14110644_170x170.png" alt="Lily Avatar" />
-                        <span className="name">Lily Kim</span>
-                    </li>
-
-                    <li>
-                        <img src="//cdn.scratch.mit.edu/get_image/user/13639421_170x170.png" alt="Tauntaun Avatar" />
-                        <span className="name">Tauntaun Kim</span>
                     </li>
 
                     <li>
