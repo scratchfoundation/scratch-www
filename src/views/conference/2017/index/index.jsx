70f99b29304f421e51f1a9ff68aa2a1a6952b62a
Sheshank Shankar <sheshank@outlook.com>
2017-11-02 20:26:27 -0700

    Update index.jsx

--- src/views/conference/2017/index/index.jsx
+++ src/views/conference/2017/index/index.jsx
@@ -218,13 +218,6 @@ var ConferenceSplash = React.createClass({
                                     <td><FormattedMessage id='conference-2017.date' /></td>
                                     <td>
                                         <FormattedDate
-                                            value={new Date(2017, 10, 10)}
-                                            year='numeric'
-                                            month='long'
-                                            day='2-digit'
-                                        />
-                                        {' - '}
-                                        <FormattedDate
                                             value={new Date(2017, 10, 12)}
                                             year='numeric'
                                             month='long'
@@ -267,7 +260,7 @@ var ConferenceSplash = React.createClass({
                                 </tr>
                             </tbody>
                         </table>
-                        <a className='button mod-2017-panel' href='mailto:admissions@treeoflifelearning.com'>
+                        <a className='button mod-2017-panel' href='https://scratchcostarica.com/'>
                             <FormattedMessage id='conference-2017.contact' />
                         </a>
                     </section>
