4f14dfcbe392a76575a0de786726ad6cb8c929b1
Sheshank Shankar <sheshank@outlook.com>
2017-11-20 06:12:12 -0800

    Fixes #1633 - Scratch Conference 2018 Website Phase 1 (#1673)
    * add 2018 conference page (new subdirectory and index page)
    
    * Redirect /conference route to 2018, and add new route for /conference/2017 for last year's conferences.
    
    * Update mailto link in the footer

---
+++ src/components/navigation/conference/2018/navigation.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var NavigationBox = require('../../base/navigation.jsx');
+
+require('./navigation.scss');
+
+var Navigation = React.createClass({
+    type: 'Navigation',
+    render: function () {
+        return (
+            <NavigationBox>
+                <ul className="ul mod-2018">
+                    <li className="li-left mod-logo mod-2018">
+                        <a href="/conference" className="logo-a">
+                            <img
+                                src="/images/logo_sm.png"
+                                alt="Scratch Logo"
+                                className="logo-a-image"
+                            />
+                            <p className="logo-a-title">Conferences</p>
+                        </a>
+                    </li>
+                </ul>
+            </NavigationBox>
+        );
+    }
+});
+
+module.exports = Navigation;
