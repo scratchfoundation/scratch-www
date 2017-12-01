5effe805721e12850fa399273d08e9ae0f709e8c
Vuton Design <vutondesign.github@gmail.com>
2017-12-01 17:14:51 +0000

    Update About Page

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var FormattedHTMLMessage = require('react-intl').FormattedHTMLMessage;
+var FormattedMessage = require('react-intl').FormattedMessage;
 var FormattedMessage = require('react-intl').FormattedMessage;
 var render = require('../../lib/render.jsx');
 
@@ -42,7 +42,7 @@ var About = React.createClass({
                         <li>
                             <h3><FormattedMessage id='about.whoUsesScratch' /></h3>
                             <img src="/images/about/who-uses-scratch.jpg" alt="" />
-                            <p><FormattedHTMLMessage id='about.whoUsesScratchDescription' /></p>
+                            <p><FormattedMessage id='about.whoUsesScratchDescription' /></p>
                         </li>
 
                         <li>
@@ -53,31 +53,66 @@ var About = React.createClass({
                                 webkitAllowFullScreen
                                 mozallowfullscreen
                                 allowFullScreen />
-                            <p><FormattedHTMLMessage id='about.literacyDescription' /></p>
+                            <p><FormattedMessage id='about.literacyDescription' /></p>
                         </li>
 
                         <li>
                             <h3><FormattedMessage id='about.aroundTheWorld' /></h3>
                             <img src="/images/about/around-the-world.png" alt="" />
-                            <p><FormattedHTMLMessage id='about.aroundTheWorldDescription' /></p>
+                            <p><FormattedMessage id='about.aroundTheWorldDescription'
+                            values={{
+                                translationLink: (
+        <a href='http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch\'>
+            <FormattedMessage id='about.translationLinkText' />
+        </a>
+    )
+                            }}
+                            />
+                            </p>
                         </li>
-
                         <li>
                             <h3><FormattedMessage id='about.schools' /></h3>
                             <img src="/images/about/scratch-in-schools.jpg" alt="" />
-                            <p><FormattedHTMLMessage id='about.schoolsDescription' /></p>
+                            <p><FormattedMessage id='about.schoolsDescription' /></p>
                         </li>
-
                         <li>
                             <h3><FormattedMessage id='about.quotes' /></h3>
                             <img src="/images/about/quotes.gif" alt="Quotes about Scratch" />
-                            <p><FormattedHTMLMessage id='about.quotesDescription' /></p>
+                            <p><FormattedMessage id='about.quotesDescription'
+                            values={{
+                                quotesLinkText: (
+            <a href='http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch\'>
+                <FormattedMessage id='about.quotesLinkText'/>
+            </a>
+        )
+                            }}
+ />
+</p>
                         </li>
 
                         <li>
                             <h3><FormattedMessage id='about.research' /></h3>
                             <img src="/images/about/research-remix.png" alt="" />
-                            <p><FormattedHTMLMessage id='about.researchDescription' /></p>
+                            <p><FormattedMessage id='about.researchDescription'
+                            values={{
+                                researchLinkText: (
+            <a href='/info/research'>
+                <FormattedMessage id='about.researchLinkText'/>
+            </a>
+        ),
+                                spfaLinkText: (
+            <a href='http://web.media.mit.edu/~mres/papers/Scratch-CACM-final.pdf'>
+                <FormattedMessage id='about.spfaLinkText'/>
+            </a>
+        ),
+                                statisticsLinkText: (
+            <a href='/statistics'>
+                <FormattedMessage id='about.statisticsLinkText'/>
+            </a>
+        )
+                            }}
+ />
+</p>
                         </li>
 
                         <li>
@@ -102,8 +137,28 @@ var About = React.createClass({
 
                         <li>
                             <h3><FormattedMessage id='about.support' /></h3>
-                            <p><FormattedHTMLMessage id='about.supportDescription' /></p>
-                        </li>
+                            <p><FormattedMessage id='about.supportDescription'
+                            values={{
+ // eslint-disable-next-line max-len supportersList: 'National Science Foundation, Scratch Foundation, Siegel Family Endowment, Google, LEGO Foundation, Intel, Cartoon Network, Lemann Foundation, MacArthur Foundation',
+                                creditslink: (
+            <a href='//scratch.mit.edu/info/credits'>
+                <FormattedMessage id='about.creditsLinkText'/>
+            </a>
+        ),
+                                donateLink: (
+            <a href='//secure.donationpay.org/scratchfoundation/'>
+                <FormattedMessage id='about.donateLinkText'/>
+            </a>
+        ),
+                                donateemail: (
+            <a href='mailto:donate@scratch.mit.edu'>
+                donate@scratch.mit.edu
+            </a>
+        )
+                            }}
+  />
+</p>
+                         </li>
                     </ul>
                 </div>
             </div>
