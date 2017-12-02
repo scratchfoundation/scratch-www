424e1915760c3120a766310df0f8a48a9ae6b4de
chrisgarrity <chrisg@media.mit.edu>
2017-12-02 16:52:13 -0500

    Fixing formatting and typos

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var FormattedMessage = require('react-intl').FormattedMessage;
-var FormattedMessage = require('react-intl').FormattedMessage;
 var render = require('../../lib/render.jsx');
 
 var Page = require('../../components/page/www/page.jsx');
@@ -59,60 +58,69 @@ var About = React.createClass({
                         <li>
                             <h3><FormattedMessage id='about.aroundTheWorld' /></h3>
                             <img src="/images/about/around-the-world.png" alt="" />
-                            <p><FormattedMessage id='about.aroundTheWorldDescription'
-                            values={{
-                                translationLink: (
-        <a href='http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch\'>
-            <FormattedMessage id='about.translationLinkText' />
-        </a>
-    )
-                            }}
-                            />
-                            </p>
+                            <p><FormattedMessage
+                                id='about.aroundTheWorldDescription'
+                                values={{
+                                    translationLink: (
+                                        <a href='http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch'>
+                                            <FormattedMessage id='about.translationLinkText' />
+                                        </a>
+                                    )
+                                }}
+                            /></p>
                         </li>
                         <li>
                             <h3><FormattedMessage id='about.schools' /></h3>
                             <img src="/images/about/scratch-in-schools.jpg" alt="" />
-                            <p><FormattedMessage id='about.schoolsDescription' /></p>
+                            <p><FormattedMessage
+                                id='about.schoolsDescription'
+                                values={{
+                                    scratchedLink: (
+                                        <a href='http://scratched.gse.harvard.edu/'>
+                                            <FormattedMessage id='about.scratchedLinkText' />
+                                        </a>
+                                    )
+                                }}
+                            /></p>
                         </li>
                         <li>
                             <h3><FormattedMessage id='about.quotes' /></h3>
                             <img src="/images/about/quotes.gif" alt="Quotes about Scratch" />
-                            <p><FormattedMessage id='about.quotesDescription'
-                            values={{
-                                quotesLinkText: (
-            <a href='http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch\'>
-                <FormattedMessage id='about.quotesLinkText'/>
-            </a>
-        )
-                            }}
- />
-</p>
+                            <p><FormattedMessage
+                                id='about.quotesDescription'
+                                values={{
+                                    quotesLink: (
+                                        <a href='http://wiki.scratch.mit.edu/wiki/How_to_Translate_Scratch'>
+                                            <FormattedMessage id='about.quotesLinkText'/>
+                                        </a>
+                                    )
+                                }}
+                            /></p>
                         </li>
 
                         <li>
                             <h3><FormattedMessage id='about.research' /></h3>
                             <img src="/images/about/research-remix.png" alt="" />
-                            <p><FormattedMessage id='about.researchDescription'
-                            values={{
-                                researchLinkText: (
-            <a href='/info/research'>
-                <FormattedMessage id='about.researchLinkText'/>
-            </a>
-        ),
-                                spfaLinkText: (
-            <a href='http://web.media.mit.edu/~mres/papers/Scratch-CACM-final.pdf'>
-                <FormattedMessage id='about.spfaLinkText'/>
-            </a>
-        ),
-                                statisticsLinkText: (
-            <a href='/statistics'>
-                <FormattedMessage id='about.statisticsLinkText'/>
-            </a>
-        )
-                            }}
- />
-</p>
+                            <p><FormattedMessage
+                                id='about.researchDescription'
+                                values={{
+                                    researchLink: (
+                                        <a href='/info/research'>
+                                            <FormattedMessage id='about.researchLinkText'/>
+                                        </a>
+                                    ),
+                                    spfaLink: (
+                                        <a href='http://web.media.mit.edu/~mres/papers/Scratch-CACM-final.pdf'>
+                                            <FormattedMessage id='about.spfaLinkText'/>
+                                        </a>
+                                    ),
+                                    statisticsLink: (
+                                        <a href='/statistics'>
+                                            <FormattedMessage id='about.statisticsLinkText'/>
+                                        </a>
+                                    )
+                                }}
+                            /></p>
                         </li>
 
                         <li>
@@ -137,29 +145,27 @@ var About = React.createClass({
 
                         <li>
                             <h3><FormattedMessage id='about.support' /></h3>
-                            <p><FormattedMessage id='about.supportDescription'
-                            values={{
- /* eslint-disable max-len */
-                                supportersList: 'National Science Foundation, Scratch Foundation, Siegel Family Endowment, Google, LEGO Foundation, Intel, Cartoon Network, Lemann Foundation, MacArthur Foundation',
-/* eslint-disable max-len */
-                                creditslink: (
-            <a href='//scratch.mit.edu/info/credits'>
-                <FormattedMessage id='about.creditsLinkText'/>
-            </a>
-        ),
-                                donateLink: (
-            <a href='//secure.donationpay.org/scratchfoundation/'>
-                <FormattedMessage id='about.donateLinkText'/>
-            </a>
-        ),
-                                donateemail: (
-            <a href='mailto:donate@scratch.mit.edu'>
-                donate@scratch.mit.edu
-            </a>
-        )
-                            }}
-  />
-</p>
+                            <p><FormattedMessage
+                                id='about.supportDescription'
+                                values={{
+                                    supportersList: 'National Science Foundation, Scratch Foundation, Siegel Family Endowment, Google, LEGO Foundation, Intel, Cartoon Network, Lemann Foundation, MacArthur Foundation', // eslint-disable-line max-len
+                                    creditsLink: (
+                                        <a href='//scratch.mit.edu/info/credits'>
+                                            <FormattedMessage id='about.creditsLinkText'/>
+                                        </a>
+                                    ),
+                                    donateLink: (
+                                        <a href='//secure.donationpay.org/scratchfoundation/'>
+                                            <FormattedMessage id='about.donateLinkText'/>
+                                        </a>
+                                    ),
+                                    donateemail: (
+                                        <a href='mailto:donate@scratch.mit.edu'>
+                                            donate@scratch.mit.edu
+                                        </a>
+                                    )
+                                }}
+                            /></p>
                          </li>
                     </ul>
                 </div>
