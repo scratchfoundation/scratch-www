e4611088ff7fd4168b8def7521604229f10eb4a4
Matthew Taylor <mewtaylor@gmail.com>
2018-01-09 12:55:36 -0500

    extract faq from published page
    after review from @thisandagain – thanks!

--- src/views/preview-faq/preview-faq.jsx
+++ src/views/preview-faq/preview-faq.jsx
@@ -1,7 +1,8 @@
+var cheerio = require('cheerio');
 var injectIntl = require('react-intl').injectIntl;
 var React = require('react');
+var xhr = require('xhr');
 
-var Box = require('../../components/box/box.jsx');
 var InformationPage = require('../../components/informationpage/informationpage.jsx');
 var Page = require('../../components/page/www/page.jsx');
 var render = require('../../lib/render.jsx');
@@ -10,18 +11,28 @@ require('./preview-faq.scss');
 
 var PreviewFaq = injectIntl(React.createClass({
     type: 'PreviewFaq',
+    getInitialState: function () {
+        return {
+            faqDoc: {__html: ''}
+        };
+    },
+    componentDidMount: function () {
+        xhr({
+            method: 'GET',
+            uri: 'https://docs.google.com/document/d/e/2PACX-1vQZFrpOagYqEwcrBBCplIomiyguPAodIJVnCq9Sr11WDI_aa2b-JtDWak-Aiu-cwWobTXftRMF6wBbd/pub?embedded=true'
+        }, function (error, response, body) {
+            var $ = cheerio.load(body);
+            this.setState({faqDoc: {__html: $('html').html()}});
+        }.bind(this));
+    },
     render: function () {
         return (
             <InformationPage title={this.props.intl.formatMessage({id: 'preview-faq.title'})}>
                 <div className="inner">
-                    <Box
-                        title={''}
-                    >
-                        <iframe
-                            className="preview-faq-iframe"
-                            src="https://docs.google.com/document/d/e/2PACX-1vQZFrpOagYqEwcrBBCplIomiyguPAodIJVnCq9Sr11WDI_aa2b-JtDWak-Aiu-cwWobTXftRMF6wBbd/pub?embedded=true"
-                        ></iframe>
-                    </Box>
+                    <div
+                        className="preview-faq"
+                        dangerouslySetInnerHTML={this.state.faqDoc}
+                    />
                 </div>
             </InformationPage>
         );
