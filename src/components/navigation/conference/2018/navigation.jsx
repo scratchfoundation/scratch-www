2989857dbed1137ff16472ebab60db3db9a7a3d7
chrisgarrity <chrisg@media.mit.edu>
2018-01-31 15:27:59 -0500

    Update Conference page to close session proposals
    Fixes #1782, needs to be hotfixed on Feb 5

--- src/components/navigation/conference/2018/navigation.jsx
+++ src/components/navigation/conference/2018/navigation.jsx
@@ -8,17 +8,48 @@ const Navigation = () => (
     <NavigationBox>
         <ul className="ul mod-2018">
             <li className="li-left mod-logo mod-2018">
-                <a
-                    className="logo-a"
-                    href="/"
-                >
-                    <img
-                        alt="Scratch Logo"
-                        className="logo-a-image"
-                        src="/images/logo_sm.png"
-                    />
-                    <p className="logo-a-title">Conferences</p>
-                </a>
+                <ul className="li-left-ul mod-2018">
+                    <li>
+                        <a
+                            className="logo-a"
+                            href="/"
+                        >
+                            <img
+                                alt="Scratch Logo"
+                                className="logo-a-image"
+                                src="/images/logo_sm.png"
+                            />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="link-a"
+                            href="/conference"
+                        >
+                            <span className="logo-a-title">Conference</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li className="li-right mod-2018">
+                <ul className="li-right-ul mod-2018">
+                    <li className="link info">
+                        <a
+                            className="link-a"
+                            href="#info"
+                        >
+                            Registration Info
+                        </a>
+                    </li>
+                    <li className="link questions">
+                        <a
+                            className="link-a"
+                            href="#questions"
+                        >
+                            Questions
+                        </a>
+                    </li>
+                </ul>
             </li>
         </ul>
     </NavigationBox>
