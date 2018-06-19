7539e744362e8c756526cfacbd79623353af7580
Connor Hudson <cohu3291@colorado.edu>
2018-06-19 16:13:25 -0400

    Add extension icons, extension info object

---
+++ src/lib/extension-info.js
@@ -0,0 +1,36 @@
+const EXTENSION_INFO = {
+    microbit: {
+        name: 'micro:bit',
+        icon: 'extension-microbit.svg',
+        hasStatus: true
+    },
+    music: {
+        name: 'Music',
+        icon: 'extension-music.svg'
+    },
+    pen: {
+        name: 'Pen',
+        icon: 'extension-pen.svg'
+    },
+    speak: {
+        name: 'Amazon Polly'
+    },
+    speech: {
+        name: 'Google Speech'
+    },
+    translate: {
+        name: 'Google Translate',
+        icon: 'extension-translate.svg'
+    },
+    videoSensing: {
+        name: 'Video Motion',
+        icon: 'extension-videomotion.svg'
+    },
+    wedo2: {
+        name: 'LEGO WeDo 2.0',
+        icon: 'extension-wedo2.svg',
+        hasStatus: true
+    }
+};
+
+export default EXTENSION_INFO;
