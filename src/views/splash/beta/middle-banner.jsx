4ee6bfcad292ebab5091401be57f030f654a9b50
Connor Hudson <cohu3291@colorado.edu>
2018-06-29 09:27:54 -0400

    Add call to action link to banners

--- src/views/splash/beta/middle-banner.jsx
+++ src/views/splash/beta/middle-banner.jsx
@@ -5,8 +5,17 @@ const React = require('react');
 const FlexRow = require('../../../components/flex-row/flex-row.jsx');
 const TitleBanner = require('../../../components/title-banner/title-banner.jsx');
 
+require('./middle-banner.scss');
+
 const MiddleBanner = () => (
-    <TitleBanner className="beta-middle-banner" />
+    <TitleBanner className="beta-middle-banner">
+        <FlexRow>
+            <a
+                className="call-to-action button"
+                href="https://beta.scratch.mit.edu/"
+            >Call to Action</a>
+        </FlexRow>
+    </TitleBanner>
 );
 
 export default MiddleBanner;
--- src/views/splash/beta/top-banner.jsx
+++ src/views/splash/beta/top-banner.jsx
@@ -5,8 +5,17 @@ const React = require('react');
 const FlexRow = require('../../../components/flex-row/flex-row.jsx');
 const TitleBanner = require('../../../components/title-banner/title-banner.jsx');
 
+require('./top-banner.scss');
+
 const TopBanner = () => (
-    <TitleBanner className="beta-top-banner" />
+    <TitleBanner className="beta-top-banner">
+        <FlexRow>
+            <a
+                className="call-to-action button"
+                href="https://beta.scratch.mit.edu/"
+            >Call to Action</a>
+        </FlexRow>
+    </TitleBanner>
 );
 
 export default TopBanner;
