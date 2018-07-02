251f7bf59d4bb66dece956172be79732dbcfc65d
Connor Hudson <cohu3291@colorado.edu>
2018-07-02 11:41:10 -0400

    Add middle banner in progress copy

--- src/views/splash/beta/middle-banner.jsx
+++ src/views/splash/beta/middle-banner.jsx
@@ -9,11 +9,13 @@ require('./middle-banner.scss');
 
 const MiddleBanner = () => (
     <TitleBanner className="beta-middle-banner">
-        <FlexRow>
+        <FlexRow className="beta-middle-container column">
+            <h2 className="beta-header">The Next Generation of Scratch</h2>
+            <h3 className="beta-copy">Try out the beta version of Scratch 3.0</h3>
             <a
-                className="call-to-action button"
+                className="beta-try-it button"
                 href="https://beta.scratch.mit.edu/"
-            >Call to Action</a>
+            >Try it!</a>
         </FlexRow>
     </TitleBanner>
 );
