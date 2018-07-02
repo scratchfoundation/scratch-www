918b4b59483ec44110ed9d0583488c5cff175dd0
Connor Hudson <cohu3291@colorado.edu>
2018-07-02 09:59:22 -0400

    Style small top banner a little more

--- src/views/splash/beta/small-top-banner.jsx
+++ src/views/splash/beta/small-top-banner.jsx
@@ -5,15 +5,16 @@ const React = require('react');
 const FlexRow = require('../../../components/flex-row/flex-row.jsx');
 const TitleBanner = require('../../../components/title-banner/title-banner.jsx');
 
-require('./top-banner.scss');
+require('./small-top-banner.scss');
 
 const SmallTopBanner = () => (
-    <TitleBanner className="beta-top-banner">
-        <FlexRow>
+    <TitleBanner className="beta-small-top-banner">
+        <FlexRow className="beta-small-header">
+            <h2 className="beta-copy">Try out the beta version of Scratch 3.0</h2>
             <a
-                className="call-to-action button"
+                className="beta-try-it button"
                 href="https://beta.scratch.mit.edu/"
-            >Call to Action</a>
+            >Try it!</a>
         </FlexRow>
     </TitleBanner>
 );
