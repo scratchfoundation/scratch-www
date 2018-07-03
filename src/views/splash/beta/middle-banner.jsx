08178a58e53da8852f0e03268c202154f1591109
Connor Hudson <cohu3291@colorado.edu>
2018-07-03 14:48:20 -0400

    Initial work on responsive middle banner

--- src/views/splash/beta/middle-banner.jsx
+++ src/views/splash/beta/middle-banner.jsx
@@ -2,8 +2,12 @@ const FormattedMessage = require('react-intl').FormattedMessage;
 const injectIntl = require('react-intl').injectIntl;
 const React = require('react');
 
+const MediaQuery = require('react-responsive').default;
+
 const FlexRow = require('../../../components/flex-row/flex-row.jsx');
 const TitleBanner = require('../../../components/title-banner/title-banner.jsx');
+
+const frameless = require('../../../lib/frameless');
 
 require('./middle-banner.scss');
 
@@ -17,8 +21,15 @@ const MiddleBanner = () => (
                 href="https://beta.scratch.mit.edu/"
             >Try it!</a>
         </FlexRow>
-        <div className="beta-banner-image">
-            <img src="/images/beta/right-illustration.png" />
+        <div className="beta-banner-images">
+            <MediaQuery maxWidth={frameless.desktop - 1}>
+                <div className="beta-banner-image left">
+                    <img src="/images/beta/left-illustration.png" />
+                </div>
+            </MediaQuery>
+            <div className="beta-banner-image right">
+                <img src="/images/beta/right-illustration.png" />
+            </div>
         </div>
     </TitleBanner>
 );
