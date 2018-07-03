4129a701e7c1945ab0aecd2686dff62d044865c4
Connor Hudson <cohu3291@colorado.edu>
2018-07-03 16:18:11 -0400

    Second pass at responsive top banner

--- src/views/splash/beta/top-banner.jsx
+++ src/views/splash/beta/top-banner.jsx
@@ -9,9 +9,6 @@ require('./top-banner.scss');
 
 const TopBanner = () => (
     <TitleBanner className="beta-top-banner">
-        <div className="beta-banner-image left">
-            <img src="/images/beta/left-illustration.png" />
-        </div>
         <FlexRow className="beta-top-container column">
             <h1 className="beta-header">The Next Generation of Scratch</h1>
             <h3 className="beta-copy">Try out the beta version of Scratch 3.0</h3>
@@ -20,8 +17,13 @@ const TopBanner = () => (
                 href="https://beta.scratch.mit.edu/"
             >Try it!</a>
         </FlexRow>
-        <div className="beta-banner-image right">
-            <img src="/images/beta/right-illustration.png" />
+        <div className="beta-banner-images">
+            <div className="beta-banner-image left">
+                <img src="/images/beta/left-illustration.png" />
+            </div>
+            <div className="beta-banner-image right">
+                <img src="/images/beta/right-illustration.png" />
+            </div>
         </div>
     </TitleBanner>
 );
