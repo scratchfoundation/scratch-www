d3cc079959cfcf91f4505f0deb54b647ed9f915a
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2018-07-19 15:46:45 -0400

    Add platform name to scratch link download button

--- src/views/microbit/microbit.jsx
+++ src/views/microbit/microbit.jsx
@@ -109,7 +109,9 @@ class MicroBit extends React.Component {
                                                 this.state.OS === this.OS_ENUM.WINDOWS ? 'windows' : 'mac'
                                             }.zip`}
                                         >
-                                            <button className="button">Download</button>
+                                            <button className="button">
+                                            Download for {this.state.OS === this.OS_ENUM.WINDOWS ? 'windows' : 'mac'}
+                                            </button>
                                         </a>
                                     </FlexRow>
                                 </FlexRow>
