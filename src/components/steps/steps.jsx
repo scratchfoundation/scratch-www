e583924ecef851931e6d2e7427e08156d48177e6
Connor Hudson <cohu3291@colorado.edu>
2018-07-24 17:01:40 -0400

    allow className prop in Steps component

--- src/components/steps/steps.jsx
+++ src/components/steps/steps.jsx
@@ -1,19 +1,21 @@
 const PropTypes = require('prop-types');
 const React = require('react');
+const classNames = require('classnames');
 
 const FlexRow = require('../../components/flex-row/flex-row.jsx');
 
 require('./steps.scss');
 
 const Steps = props => (
-    <FlexRow className="steps">
+    <FlexRow className={classNames('steps', props.className)}>
         {/* TODO: Should this component do something with automatically numbering individual steps? */}
         {props.children}
     </FlexRow>
 );
 
 Steps.propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    className: PropTypes.string
 };
 
 module.exports = Steps;
