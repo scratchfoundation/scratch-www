f0e5b433605b49b0798dab6d9b961d2eac2d7924
Connor Hudson <cohu3291@colorado.edu>
2018-07-25 09:41:44 -0400

    imageUrl -> imageSrc in ProjectCard

--- src/components/extension-landing/project-card.jsx
+++ src/components/extension-landing/project-card.jsx
@@ -8,7 +8,7 @@ const ProjectCard = props => (
         href={props.cardUrl}
     >
         <div className="project-card-image">
-            <img src={props.imageUrl} />
+            <img src={props.imageSrc} />
         </div>
         <div className="project-card-info">
             <h4>{props.title}</h4>
@@ -20,7 +20,7 @@ const ProjectCard = props => (
 ProjectCard.propTypes = {
     cardUrl: PropTypes.string,
     description: PropTypes.string,
-    imageUrl: PropTypes.string,
+    imageSrc: PropTypes.string,
     title: PropTypes.string
 };
 
