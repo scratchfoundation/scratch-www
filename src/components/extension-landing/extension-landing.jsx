fc6a5215a851f58fb7d17721a5ea2c418056aa5b
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2018-08-16 13:52:51 -0400

    Detect mac vs windows

--- src/components/extension-landing/extension-landing.jsx
+++ src/components/extension-landing/extension-landing.jsx
@@ -10,8 +10,15 @@ class ExtensionLanding extends React.Component {
             'onSetOS'
         ]);
 
+        let detectedOS = OS_ENUM.WINDOWS;
+        if (window.navigator && window.navigator.platform) {
+            if (window.navigator.platform === 'MacIntel') {
+                detectedOS = OS_ENUM.MACOS;
+            }
+        }
+
         this.state = {
-            OS: OS_ENUM.WINDOWS
+            OS: detectedOS
         };
     }
 
