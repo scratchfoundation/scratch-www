8e6af5963561426ece2abb04c2605c9e56a087f4
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2018-12-10 18:06:07 -0500

    Initial draft of ideas page

--- src/components/ttt-tile/ttt-tile.jsx
+++ src/components/ttt-tile/ttt-tile.jsx
@@ -7,62 +7,34 @@ require('../forms/button.scss');
 require('./ttt-tile.scss');
 
 const TTTTile = props => (
-    <div className={classNames('ttt-tile', props.className)}>
-        <a href={props.tutorialLoc}>
-            <div className="ttt-tile-tutorial">
-                <div className="ttt-tile-image">
-                    <img
-                        alt=""
-                        className="ttt-tile-image-img"
-                        src={props.thumbUrl}
-                    />
-                    <div className="ttt-tile-image-try">
-                        <div className="button mod-ttt-try-button">
-                            <FormattedMessage id="tile.tryIt" />
-                        </div>
-                    </div>
-                </div>
-                <div className="ttt-tile-info">
-
-                    <div className="ttt-tile-tag">
-                        <FormattedMessage
-                            defaultMessage="Tutorial"
-                            id="ttt.tutorial"
-                        />
-                    </div>
-                    <h4 className="ttt-tile-title">{props.title}</h4>
-                    <p className="ttt-tile-description">
-                        {props.description}
-                    </p>
-                </div>
-            </div>
-
-        </a>
-        {props.onGuideClick && (
-            <div
-                className="ttt-tile-guides"
-                onClick={props.onGuideClick}
-            >
-                <FormattedMessage
-                    defaultMessage="See Cards and Guides"
-                    id="tile.guides"
-                />
+    <div
+        className={classNames('ttt-tile', props.className)}
+        onClick={props.onClick}
+    >
+        <div className="ttt-tile-tutorial">
+            <div className="ttt-tile-image">
                 <img
-                    className="ttt-tile-open-modal"
-                    src="/svgs/modal/open-blue.svg"
+                    alt=""
+                    className="ttt-tile-image-img"
+                    src={props.thumbImage}
                 />
             </div>
-        )}
+            <div className="ttt-tile-info">
+                <h4 className="ttt-tile-title">{props.title}</h4>
+                <p className="ttt-tile-description">
+                    {props.description}
+                </p>
+            </div>
+        </div>
     </div>
 );
 
 TTTTile.propTypes = {
     className: PropTypes.string,
     description: PropTypes.string,
-    onGuideClick: PropTypes.func,
-    thumbUrl: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    tutorialLoc: PropTypes.string.isRequired
+    onClick: PropTypes.func,
+    thumbImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
 };
 
 module.exports = TTTTile;
---
+++ src/views/ideas/ideas.jsx
@@ -0,0 +1,248 @@
+const bindAll = require('lodash.bindall');
+const FormattedHTMLMessage = require('react-intl').FormattedHTMLMessage;
+const FormattedMessage = require('react-intl').FormattedMessage;
+const injectIntl = require('react-intl').injectIntl;
+const intlShape = require('react-intl').intlShape;
+const React = require('react');
+
+const Button = require('../../components/forms/button.jsx');
+const FlexRow = require('../../components/flex-row/flex-row.jsx');
+const MasonryGrid = require('../../components/masonrygrid/masonrygrid.jsx');
+const TitleBanner = require('../../components/title-banner/title-banner.jsx');
+const TTTModal = require('../../components/modal/ttt/modal.jsx');
+const TTTTile = require('../../components/ttt-tile/ttt-tile.jsx');
+
+const Page = require('../../components/page/www/page.jsx');
+const render = require('../../lib/render.jsx');
+
+const Tiles = require('./ttt.json');
+
+require('./ideas.scss');
+
+class Ideas extends React.Component {
+    constructor (props) {
+        super(props);
+        bindAll(this, [
+            'handleShowTTTModal',
+            'handleHideTTTModal',
+            'renderTTTTiles'
+        ]);
+        this.state = {
+            currentTile: Tiles[0],
+            TTTModalOpen: false
+        };
+    }
+    handleShowTTTModal (tile) {
+        // expects translated tile
+        this.setState({
+            currentTile: tile,
+            TTTModalOpen: true
+        });
+    }
+    handleHideTTTModal () {
+        this.setState({
+            TTTModalOpen: false
+        });
+    }
+    renderTTTTiles () {
+        return Tiles.map((tile, key) => {
+            const translatedTile = {
+                tutorialUrl: this.props.intl.formatMessage({id: tile.tutorialUrl}),
+                modalImage: tile.modalImage,
+                description: this.props.intl.formatMessage({id: tile.description}),
+                guideUrl: this.props.intl.formatMessage({id: tile.guideUrl}),
+                thumbImage: tile.thumbImage,
+                title: this.props.intl.formatMessage({id: tile.title}),
+                cardsUrl: tile.cardsUrl
+            };
+            return (
+                <TTTTile
+                    key={key}
+                    onClick={() => { // eslint-disable-line react/jsx-no-bind
+                        this.handleShowTTTModal(translatedTile);
+                    }}
+                    {...translatedTile}
+                />
+            );
+        });
+    }
+    render () {
+        return (
+            <div>
+                <div className="banner-wrapper">
+                    <TitleBanner className="masthead ideas-banner">
+                        <div className="title-banner-p">
+                            <img src="/images/ideas/masthead-illustration.svg" />
+                            <h1 className="title-banner-h1">
+                                <FormattedMessage id="ideas.headerMessage" />
+                            </h1>
+                            <a href="">
+                                <Button className="ideas-button">
+                                    <img src="/images/ideas/bulb-icon.svg" />
+                                    <FormattedMessage id="ideas.headerButtonMessage" />
+                                </Button>
+                            </a>
+                        </div>
+                    </TitleBanner>
+                </div>
+                <div className="inner">
+                    <FlexRow
+                        as="section"
+                        className="tips-info-section"
+                    >
+                        <div>
+                            <img src="images/ideas/getting-started-illustration.svg" />
+                        </div>
+                        <div>
+                            <h2>
+                                <FormattedMessage id="ideas.gettingStartedTitle" />
+                            </h2>
+                            <p>
+                                <FormattedHTMLMessage id="ideas.gettingStartedText" />
+                            </p>
+                            <a href="">
+                                <Button className="ideas-button">
+                                    <img src="/images/ideas/try-it-icon.svg" />
+                                    <FormattedMessage id="ideas.tryIt" />
+                                </Button>
+                            </a>
+                        </div>
+                    </FlexRow>
+                </div>
+                <div className="inner">
+                    <section className="ttt-section">
+                        <div className="ttt-head">
+                            <h2>
+                                <FormattedMessage id="ideas.activityGuidesTitle" />
+                            </h2>
+                            <p>
+                                <FormattedHTMLMessage id="ideas.activityGuidesText" />
+                            </p>
+                        </div>
+                        <MasonryGrid >
+                            {this.renderTTTTiles()}
+                        </MasonryGrid>
+                        <TTTModal
+                            isOpen={this.state.TTTModalOpen}
+                            onRequestClose={this.handleHideTTTModal}
+                            {...this.state.currentTile}
+                        />
+                        <a
+                            className="wide-button"
+                            href=""
+                        >
+                            <Button className="ideas-button wide-button">
+                                <FormattedMessage id="ideas.seeAllTutorials" />
+                            </Button>
+                        </a>
+                    </section>
+                </div>
+                <div>
+                    <div className="inner">
+                        <FlexRow
+                            as="section"
+                            className="tips-info-section cards-info"
+                        >
+                            <div className="tips-info-body">
+                                <h2>
+                                    <FormattedMessage id="ideas.cardsTitle" />
+                                </h2>
+                                <p>
+                                    <FormattedHTMLMessage id="ideas.cardsText" />
+                                </p>
+                                <a
+                                    href={this.props.intl.formatMessage({
+                                        id: 'cards.ScratchCardsAllLink'
+                                    })}
+                                >
+                                    <Button className="tips-button">
+                                        <img src="/images/ideas/download-icon.svg" />
+                                        <FormattedMessage id="ideas.downloadPDF" />
+                                    </Button>
+                                </a>
+                            </div>
+                            <div className="tips-info-body tips-illustration">
+                                <img src="/images/ideas/cards-illustration.svg" />
+                            </div>
+                        </FlexRow>
+                    </div>
+                </div>
+                <div className="inner">
+                    <div className="tips-divider" />
+                </div>
+                <div className="tips-resources">
+                    <div className="inner">
+                        <FlexRow
+                            as="section"
+                            className="tips-info-section"
+                        >
+                            <div className="tips-info-body tips-illustration">
+                                <img
+                                    className="mod-flow-left"
+                                    src="/images/ideas/starter-projects-illustration.svg"
+                                />
+                            </div>
+                            <div className="tips-info-body">
+                                <h2>
+                                    <FormattedMessage id="ideas.starterProjectsTitle" />
+                                </h2>
+                                <p>
+                                    <FormattedHTMLMessage id="ideas.starterProjectsText" />
+                                </p>
+                                <p>
+                                    <a href="/starter_projects">
+                                        <Button className="tips-button">
+                                            <FormattedMessage id="ideas.starterProjectsButton" />
+                                        </Button>
+                                    </a>
+                                </p>
+                            </div>
+                        </FlexRow>
+                    </div>
+                </div>
+                <div className="gray-area">
+                    <div className="inner">
+                        <FlexRow
+                            as="section"
+                            className="tips-info-section mod-align-top"
+                        >
+                            <div className="tips-info-body mod-narrow">
+                                <img
+                                    className="tips-icon"
+                                    src="/images/tips/download-icon.svg"
+                                />
+                                <h3>
+                                    <FormattedMessage id="ideas.offlineEditorHeader" />
+                                </h3>
+                                <p>
+                                    <FormattedHTMLMessage id="ideas.offlineEditorBody" />
+                                </p>
+                            </div>
+                            <div className="tips-info-body mod-narrow">
+                                <img
+                                    className="tips-icon"
+                                    src="/images/tips/question-icon.svg"
+                                />
+                                <h3>
+                                    <FormattedMessage id="ideas.questionsHeader" />
+                                </h3>
+                                <p>
+                                    <FormattedHTMLMessage id="ideas.questionsBody" />
+                                </p>
+                            </div>
+                        </FlexRow>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Ideas.propTypes = {
+    intl: intlShape
+};
+
+const WrappedIdeas = injectIntl(Ideas);
+
+render(
+    <Page><WrappedIdeas /></Page>, document.getElementById('app'));
