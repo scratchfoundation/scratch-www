2020759204c4a7f35a9d291dbecaa4348be45247
seotts <sotts@scratch.mit.edu>
2019-02-26 14:04:56 -0500

    updated mod job description

--- src/views/jobs/moderator/moderator.jsx
+++ src/views/jobs/moderator/moderator.jsx
@@ -9,65 +9,86 @@ const Moderator = () => (
     <InformationPage title={'Community Moderator'}>
         <div className="inner info-inner">
             <p>
-                Interested in kids, creativity, and online communities?
-                We&#39;re seeking community moderators to work with the Scratch
-                Team. Moderators will support creative activities and
-                positive interactions on Scratch — a free online community
-                where young people program and share interactive stories,
-                games, and animations. Scratch has grown to more than 20
-                million registered members (ages 8 and up), creating and
-                sharing thousands of projects each day. Moderators will
-                gain valuable experience working online with youth in a
-                creative, interest driven setting.
+                Interested in online communities, internet culture, and
+                working with kids? We&#39;re seeking community moderators
+                to work with the <a href="/">Scratch</a> Team. Scratch
+                Moderators help keep Scratch a safe, trusted, and friendly
+                environment.
+            </p>
+            <p>
+                Scratch is a free service where young people learn to
+                program by creating interactive stories, games, and
+                animations which they can share to an online community.
+                Scratch has grown to more than 30 million registered
+                members (ages 8 and up) who create and share thousands
+                of projects each day. Moderators will gain valuable
+                experience working online with youth in a creative,
+                interest-driven setting.
             </p>
             <h3>Responsibilities:</h3>
             <ul>
                 <li>
-                    Participate actively in the Scratch online community as
-                    a mentor and resource for youth
+                    Promote the values and core ideas of the Scratch
+                    project (such as inclusiveness, creative collaboration,
+                    and constructive feedback)
+
                 </li>
                 <li>
-                    Help moderate projects, studios, and comments on the
-                    website
+                    Review reported users, projects, studios, and comments
+                    for appropriateness on the Scratch website
+
                 </li>
                 <li>
-                    Support youth volunteer programs in the online community
+                    Work with young people to help them understand what they
+                    did that breaks our <a href="/community_guidelines">Community Guidelines</a>
                 </li>
                 <li>
-                    Promote the values and core ideas of the Scratch project
-                    (such as remixing, creative collaboration, and
-                    constructive feedback)
+                    Determine when a person is or isn’t likely to be able to
+                    participate constructively in the Scratch online community
                 </li>
             </ul>
             <br />
             <h3>Qualifications:</h3>
             <ul>
                 <li>
-                    Active participation in online communities, forums, or
-                    other web-based media
+                    Active participation in online communities, forums,
+                    or social media
                 </li>
                 <li>
                     Excellent writing and communication skills
                 </li>
                 <li>
-                    Good at considering issues from multiple perspectives
+                    Comfortable learning to use new software and websites
                 </li>
                 <li>
-                    Able to work independently and as part of a team
+                    Strong online research skills
                 </li>
                 <li>
-                    Interest in visual arts, programming, or teaching
+                    Good judgement and ability to consider issues from
+                    multiple perspectives
                 </li>
                 <li>
-                    Not required, but would be cool: Ability to speak another
-                    language
+                    Able to work well independently, manage your time
+                    well, and stay focused while working remotely
+                </li>
+                <li>
+                    Able to work well as part of a team and participate
+                    in group discussions
+                </li>
+                <li>
+                    Not required, but would be nice: ability to read and
+                    write in multiple languages (especially if one of those
+                    languages is Japanese, Korean, or Russian!)
                 </li>
             </ul>
             <br />
             <p>
-                This position is part-time (10-12 hours per week) under contract.
-                All candidates must be at least 18 years old and have
-                authorization to work in the United States.
+                This position is part-time (10-15 hours per week),
+                flexible hours (this work can be done 24/7 and we
+                set a schedule which fits in with your life),
+                under contract with starting pay of $12 per hour.
+                All candidates must be at least 18 years old and
+                have authorization to work in the United States.
             </p>
             <p><b>(MIT Media Lab, Cambridge, MA or Remote)</b></p>
             <p>
