eac512da92f8984695463b6a5fc7948ec16197ec
picklesrus <picklesrus@users.noreply.github.com>
2019-08-08 16:20:48 -0400

    Add standalone registration page that just contains the JoinFlowModal.

---
+++ src/views/register/register.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const render = require('../../lib/render.jsx');
+const JoinModal = require('../../components/modal/join/modal.jsx');
+
+const openModal = true;
+const Register = () => (
+    <div className="register">
+        <JoinModal
+            isOpen={openModal}
+            key="scratch3registration"
+        />
+    </div>
+);
+render(<Register />, document.getElementById('app'));
