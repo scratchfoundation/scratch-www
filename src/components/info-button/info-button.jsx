3f7681821e21ed64346ecf0e2f93837d325a671b
Ben Wheeler <wheeler.benjamin@gmail.com>
2019-08-11 15:00:32 -0400

    info button in join flow: add support for, formatting of

--- src/components/info-button/info-button.jsx
+++ src/components/info-button/info-button.jsx
@@ -1,18 +1,46 @@
+const bindAll = require('lodash.bindall');
 const PropTypes = require('prop-types');
 const React = require('react');
 
 require('./info-button.scss');
 
-const InfoButton = ({
-    message
-}) => (
-    <div className="info-button">
-        ?
-    </div>
-);
+class InfoButton extends React.Component {
+    constructor (props) {
+        super(props);
+        bindAll(this, [
+            'handleHideMessage',
+            'handleShowMessage'
+        ]);
+        this.state = {
+            visible: false
+        };
+    }
+    handleHideMessage () {
+        this.setState({visible: false});
+    }
+    handleShowMessage () {
+        this.setState({visible: true});
+    }
+    render () {
+        return (
+            <div
+                className="info-button"
+                onClick={this.handleShowMessage}
+                onMouseOut={this.handleHideMessage}
+                onMouseOver={this.handleShowMessage}
+            >
+                {this.state.visible && (
+                    <div className="info-button-message">
+                        {this.props.message}
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
 
 InfoButton.propTypes = {
-    message: PropTypes.string,
+    message: PropTypes.string
 };
 
 module.exports = InfoButton;
