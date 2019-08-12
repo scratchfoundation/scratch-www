692d24b36278efa05dc06f369fb90a59027859b4
picklesrus <picklesrus@users.noreply.github.com>
2019-08-12 14:29:32 -0400

    try adding build config

--- src/views/register/register.jsx
+++ src/views/register/register.jsx
@@ -1,14 +1,20 @@
 const React = require('react');
 const render = require('../../lib/render.jsx');
 const JoinModal = require('../../components/modal/join/modal.jsx');
+const ErrorBoundary = require('../../components/errorboundary/errorboundary.jsx');
 
 const openModal = true;
 const Register = () => (
-    <div className="register">
-        <JoinModal
-            isOpen={openModal}
-            key="scratch3registration"
-        />
-    </div>
+    <ErrorBoundary>
+        <div className="page">
+            <div className="register">
+                <JoinModal
+                    isOpen={openModal}
+                    key="scratch3registration"
+                />
+            </div>
+        </div>
+    </ErrorBoundary>
 );
+
 render(<Register />, document.getElementById('app'));
