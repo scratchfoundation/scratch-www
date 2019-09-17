50821f8de70bf0923ce8ce30c064bb33f964914b
picklesrus <picklesrus@users.noreply.github.com>
2019-09-16 20:09:37 -0400

    Version of standalone join flow page without Modal.

--- src/views/join/join.jsx
+++ src/views/join/join.jsx
@@ -1,17 +1,22 @@
 const React = require('react');
 const render = require('../../lib/render.jsx');
-const JoinModal = require('../../components/modal/join/modal.jsx');
+const JoinFlow = require('../../components/join-flow/join-flow.jsx');
 const ErrorBoundary = require('../../components/errorboundary/errorboundary.jsx');
 // Require this even though we don't use it because, without it, webpack runs out of memory...
 const Page = require('../../components/page/www/page.jsx'); // eslint-disable-line no-unused-vars
+require('./join.scss');
 
 const openModal = true;
 const Register = () => (
     <ErrorBoundary>
-        <JoinModal
-            isOpen={openModal}
-            key="scratch3registration"
-        />
+        <div
+            className="join-standalone"
+        >
+            <JoinFlow
+                isOpen={openModal}
+                key="scratch3registration"
+            />
+        </div>
     </ErrorBoundary>
 );
 render(<Register />, document.getElementById('app'));
