fd131b84c15f5410cd7ca3f059fd725d45c5ff6d
picklesrus <picklesrus@users.noreply.github.com>
2019-09-23 18:04:59 -0400

    Add Logo to join flow standalone page. Will need some css work, especially for small screens.

--- src/views/join/join.jsx
+++ src/views/join/join.jsx
@@ -5,8 +5,21 @@ const ErrorBoundary = require('../../components/errorboundary/errorboundary.jsx'
 // Require this even though we don't use it because, without it, webpack runs out of memory...
 const Page = require('../../components/page/www/page.jsx'); // eslint-disable-line no-unused-vars
 
+require('./join.scss');
 const Register = () => (
     <ErrorBoundary>
+        <div className="join">
+            <a
+                aria-label="Scratch"
+                href="/"
+            >
+                <img
+                    className="logo"
+                    src="/images/logo_sm.png"
+                />
+            </a>
+
+        </div>
         <JoinModal
             isOpen
             key="scratch3registration"
