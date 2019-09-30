4a2553e8194ce92db215ef40a96b410cb7250b96
Ben Wheeler <wheeler.benjamin@gmail.com>
2019-09-30 10:48:10 -0400

    create sentry library for initing Sentry

---
+++ src/lib/sentry.js
@@ -0,0 +1,18 @@
+const initSentry = () => {
+    // initialize Sentry instance, making sure it hasn't been initialized already
+    if (!window.Sentry && `${process.env.SENTRY_DSN}` !== '') {
+        const Sentry = require('@sentry/browser');
+
+        Sentry.init({
+            dsn: `${process.env.SENTRY_DSN}`,
+            // Do not collect global onerror, only collect specifically from React error boundaries.
+            // TryCatch plugin also includes errors from setTimeouts (i.e. the VM)
+            integrations: integrations => integrations.filter(i =>
+                !(i.name === 'GlobalHandlers' || i.name === 'TryCatch'))
+        });
+
+        window.Sentry = Sentry; // Allow GUI access to Sentry via window
+    }
+};
+
+module.exports = initSentry;
