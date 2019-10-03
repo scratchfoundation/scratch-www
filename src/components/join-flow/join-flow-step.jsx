4a328f0408592aa4d82e89b45c63732c0afc2d19
Ben Wheeler <wheeler.benjamin@gmail.com>
2019-10-03 12:42:17 -0400

    make join flow step scroll on mount

--- src/components/join-flow/join-flow-step.jsx
+++ src/components/join-flow/join-flow-step.jsx
@@ -8,72 +8,82 @@ const ModalInnerContent = require('../modal/base/modal-inner-content.jsx');
 
 require('./join-flow-step.scss');
 
-const JoinFlowStep = ({
-    children,
-    innerClassName,
-    description,
-    descriptionClassName,
-    footerContent,
-    headerImgSrc,
-    nextButton,
-    onSubmit,
-    title,
-    titleClassName,
-    waiting
-}) => (
-    <form onSubmit={onSubmit}>
-        <div className="join-flow-outer-content">
-            {headerImgSrc && (
-                <div className="join-flow-header-image-wrapper">
-                    <img
-                        className="join-flow-header-image"
-                        src={headerImgSrc}
-                    />
-                </div>
-            )}
-            <div>
-                <ModalInnerContent
-                    className={classNames(
-                        'join-flow-inner-content',
-                        innerClassName
-                    )}
-                >
-                    {title && (
-                        <ModalTitle
-                            className={classNames(
-                                'join-flow-title',
-                                titleClassName
-                            )}
-                            title={title}
-                        />
-                    )}
-                    {description && (
-                        <div
-                            className={classNames(
-                                'join-flow-description',
-                                descriptionClassName
-                            )}
-                        >
-                            {description}
+class JoinFlowStep extends React.Component {
+    componentDidMount () {
+        const progressionEls = document.getElementsByClassName('progression');
+        const progressionEl = progressionEls[0];
+        progressionEl.scrollIntoView();
+    }
+    render () {
+        const {
+            children,
+            innerClassName,
+            description,
+            descriptionClassName,
+            footerContent,
+            headerImgSrc,
+            nextButton,
+            onSubmit,
+            title,
+            titleClassName,
+            waiting
+        } = this.props;
+        return (
+            <form onSubmit={onSubmit}>
+                <div className="join-flow-outer-content">
+                    {headerImgSrc && (
+                        <div className="join-flow-header-image-wrapper">
+                            <img
+                                className="join-flow-header-image"
+                                src={headerImgSrc}
+                            />
                         </div>
                     )}
-                    {children}
-                </ModalInnerContent>
-            </div>
-            <div>
-                {footerContent && (
-                    <div className="join-flow-footer-message">
-                        {footerContent}
+                    <div>
+                        <ModalInnerContent
+                            className={classNames(
+                                'join-flow-inner-content',
+                                innerClassName
+                            )}
+                        >
+                            {title && (
+                                <ModalTitle
+                                    className={classNames(
+                                        'join-flow-title',
+                                        titleClassName
+                                    )}
+                                    title={title}
+                                />
+                            )}
+                            {description && (
+                                <div
+                                    className={classNames(
+                                        'join-flow-description',
+                                        descriptionClassName
+                                    )}
+                                >
+                                    {description}
+                                </div>
+                            )}
+                            {children}
+                        </ModalInnerContent>
                     </div>
-                )}
-                <NextStepButton
-                    content={nextButton}
-                    waiting={waiting}
-                />
-            </div>
-        </div>
-    </form>
-);
+                    <div>
+                        {footerContent && (
+                            <div className="join-flow-footer-message">
+                                {footerContent}
+                            </div>
+                        )}
+                        <NextStepButton
+                            content={nextButton}
+                            waiting={waiting}
+                        />
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
 
 JoinFlowStep.propTypes = {
     children: PropTypes.node,
