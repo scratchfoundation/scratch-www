166a2dc282d2b8d31690959958cc5110eb13334e
picklesrus <picklesrus@users.noreply.github.com>
2019-12-09 16:27:47 -0500

    Initial landing page with info about Scratch conference 2020.
    Also switches the /conference redirect to go to conference/2020 instead 
    of /2019.

---
+++ src/components/footer/conference/2020/footer.jsx
@@ -0,0 +1,158 @@
+const FormattedMessage = require('react-intl').FormattedMessage;
+const injectIntl = require('react-intl').injectIntl;
+const intlShape = require('react-intl').intlShape;
+const React = require('react');
+
+const FlexRow = require('../../../flex-row/flex-row.jsx');
+const FooterBox = require('../../container/footer.jsx');
+const LanguageChooser = require('../../../languagechooser/languagechooser.jsx');
+
+require('../footer.scss');
+
+const ConferenceFooter = props => (
+    <FooterBox>
+        <FlexRow className="scratch-links">
+            <div className="family">
+                <h4><FormattedMessage id="footer.scratchFamily" /></h4>
+                <FlexRow>
+                    <FlexRow
+                        as="ul"
+                        className="column"
+                    >
+                        <li>
+                            <a
+                                href="https://scratch.mit.edu"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                Scratch
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.scratchjr.org/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                ScratchJr
+                            </a>
+                        </li>
+                    </FlexRow>
+                    <FlexRow
+                        as="ul"
+                        className="column"
+                    >
+                        <li>
+                            <a
+                                href="http://www.scratchfoundation.org/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                Scratch Foundation
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="http://scratched.gse.harvard.edu/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                ScratchEd
+                            </a>
+                        </li>
+                    </FlexRow>
+                    <FlexRow
+                        as="ul"
+                        className="column"
+                    >
+                        <li>
+                            <a
+                                href="http://day.scratch.mit.edu"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                Scratch Day
+                            </a>
+                        </li>
+                    </FlexRow>
+                </FlexRow>
+            </div>
+            <div className="media">
+                <div className="contact-us">
+                    <h4>Contact</h4>
+                    <p>
+                        <a
+                            href="mailto:conference@scratch.mit.edu"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            Email Us
+                        </a>
+                    </p>
+                </div>
+                <div className="social">
+                    <FlexRow as="ul">
+                        <li>
+                            <a
+                                href="//www.twitter.com/scratch"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                <img
+                                    alt="scratch twitter"
+                                    src="/images/conference/footer/twitter.png"
+                                />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="//www.facebook.com/scratchteam"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                <img
+                                    alt="scratch facebook"
+                                    src="/images/conference/footer/facebook.png"
+                                />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://medium.com/scratchteam-blog"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                <img
+                                    alt="scratch team blog"
+                                    src="/images/conference/footer/medium.png"
+                                />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://scratch-foundation.myshopify.com/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                <img
+                                    alt="scratch store"
+                                    src="/images/conference/footer/shopify-white.svg"
+                                />
+                            </a>
+                        </li>
+                    </FlexRow>
+                </div>
+            </div>
+        </FlexRow>
+        <div className="conf2020-organized">
+            <FormattedMessage id="conference-2020.organizedBy" />
+        </div>
+        <LanguageChooser locale={props.intl.locale} />
+    </FooterBox>
+);
+
+ConferenceFooter.propTypes = {
+    intl: intlShape
+};
+
+module.exports = injectIntl(ConferenceFooter);
---
+++ src/components/page/conference/2020/page.jsx
@@ -0,0 +1,27 @@
+const PropTypes = require('prop-types');
+const React = require('react');
+
+const Navigation = require('../../../navigation/conference/2020/navigation.jsx');
+const Footer = require('../../../footer/conference/2020/footer.jsx');
+
+require('../page.scss');
+
+const Page = props => (
+    <div className="page mod-conference">
+        <div id="navigation">
+            <Navigation />
+        </div>
+        <div id="view">
+            {props.children}
+        </div>
+        <div id="footer">
+            <Footer />
+        </div>
+    </div>
+);
+
+Page.propTypes = {
+    children: PropTypes.node
+};
+
+module.exports = Page;
