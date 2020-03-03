3dc5155c381e14ed100381988f691d167bb531db
Chris Garrity <chrisg@media.mit.edu>
2020-03-03 10:10:20 -0500

    Revised wording

--- src/views/contact-us/contact-us.jsx
+++ src/views/contact-us/contact-us.jsx
@@ -21,22 +21,26 @@ const ContactUs = injectIntl(props => (
                         <a href="/faq"><FormattedMessage id="contactUs.faqLinkText" /></a>
                     )}}
                 /></p>
-                <p><FormattedMessage
-                    id="contactUs.forumsInfo"
-                    values={{forumsLink: (
-                        <a href="/discuss/"><FormattedMessage id="contactUs.forumsLinkText" /></a>
-                    )}}
-                /></p>
-                <p><FormattedMessage
-                    id="contactUs.forumsAsk"
-                    values={{forumsLink: (
-                        <a href="/discuss/"><FormattedMessage id="contactUs.forumsLinkText" /></a>
-                    )}}
-                /></p>
+                <p><FormattedMessage id="contactUs.forumsInfo" /></p>
                 <ul>
-                    <li><a href="/discuss/4/"><FormattedMessage id="contactUs.questionsForum" /></a></li>
-                    <li><a href="/discuss/7/"><FormattedMessage id="contactUs.scriptsForum" /></a></li>
-                    <li><a href="/discuss/3/"><FormattedMessage id="contactUs.bugsForum" /></a></li>
+                    <li><FormattedMessage
+                        id="contactUs.questionsForum"
+                        values={{questionsLink: (
+                            <a href="/discuss/4/"><FormattedMessage id="contactUs.questionsLinkText" /></a>
+                        )}}
+                    /></li>
+                    <li><FormattedMessage
+                        id="contactUs.scriptsForum"
+                        values={{scriptsLink: (
+                            <a href="/discuss/4/"><FormattedMessage id="contactUs.scriptsLinkText" /></a>
+                        )}}
+                    /></li>
+                    <li><FormattedMessage
+                        id="contactUs.bugsForum"
+                        values={{bugsLink: (
+                            <a href="/discuss/4/"><FormattedMessage id="contactUs.bugsLinkText" /></a>
+                        )}}
+                    /></li>
                 </ul>
                 <p><FormattedMessage id="contactUs.formIntro" /></p>
                 <HelpForm title={props.intl.formatMessage({id: 'contactUs.contactScratch'})} />
