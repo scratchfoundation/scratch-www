bcb848d515f86142e5ea8d71b4e82ca6829aae7a
picklesrus <picklesrus@users.noreply.github.com>
2020-05-01 10:27:31 -0400

    Remove now unused FormatedHTMLMessage.

--- src/views/teachers/landing/landing.jsx
+++ src/views/teachers/landing/landing.jsx
@@ -1,4 +1,3 @@
-const FormattedHTMLMessage = require('react-intl').FormattedHTMLMessage;
 const FormattedMessage = require('react-intl').FormattedMessage;
 const injectIntl = require('react-intl').injectIntl;
 const intlShape = require('react-intl').intlShape;
