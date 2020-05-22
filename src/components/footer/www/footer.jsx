fe58a4c30b4f2d7e41212e5d0a502128e210444c
Boomer001 <60622217+BoomerScratch@users.noreply.github.com>
2020-05-22 15:39:31 +0200

    Use scratch-wiki.info in the footer

--- src/components/footer/www/footer.jsx
+++ src/components/footer/www/footer.jsx
@@ -108,7 +108,7 @@ const Footer = props => (
                         </a>
                     </dd>
                     <dd>
-                        <a href="https://en.scratch-wiki.info/">
+                        <a href="https://scratch-wiki.info/">
                             <FormattedMessage id="general.wiki" />
                         </a>
                     </dd>
