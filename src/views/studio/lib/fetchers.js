f40f3fa7c7718b13fbe45d4d2c83d64f169dd0d7
Paul Kaplan <pkaplan@media.mit.edu>
2021-03-18 11:31:28 -0400

    Rename variables

--- src/views/studio/lib/fetchers.js
+++ src/views/studio/lib/fetchers.js
@@ -1,27 +1,27 @@
 const ITEM_LIMIT = 4;
 
 const infoFetcher = studioId => fetch(`${process.env.API_HOST}/studios/${studioId}`)
-    .then(d => d.json());
+    .then(response => response.json());
 
 const projectFetcher = (studioId, offset) =>
     fetch(`${process.env.API_HOST}/studios/${studioId}/projects?limit=${ITEM_LIMIT}&offset=${offset}`)
-        .then(d => d.json())
-        .then(d => ({items: d, moreToLoad: d.length === ITEM_LIMIT}));
+        .then(response => response.json())
+        .then(data => ({items: data, moreToLoad: data.length === ITEM_LIMIT}));
 
 const curatorFetcher = (studioId, offset) =>
     fetch(`${process.env.API_HOST}/studios/${studioId}/curators?limit=${ITEM_LIMIT}&offset=${offset}`)
-        .then(d => d.json())
-        .then(d => ({items: d, moreToLoad: d.length === ITEM_LIMIT}));
+        .then(response => response.json())
+        .then(data => ({items: data, moreToLoad: data.length === ITEM_LIMIT}));
 
 const managerFetcher = (studioId, offset) =>
     fetch(`${process.env.API_HOST}/studios/${studioId}/managers?limit=${ITEM_LIMIT}&offset=${offset}`)
-        .then(d => d.json())
-        .then(d => ({items: d, moreToLoad: d.length === ITEM_LIMIT}));
+        .then(response => response.json())
+        .then(data => ({items: data, moreToLoad: data.length === ITEM_LIMIT}));
 
 const activityFetcher = studioId =>
     fetch(`${process.env.API_HOST}/studios/${studioId}/activity`)
-        .then(d => d.json())
-        .then(d => ({items: d, moreToLoad: false})); // No pagination on the activity feed
+        .then(response => response.json())
+        .then(data => ({items: data, moreToLoad: false})); // No pagination on the activity feed
 
 export {
     activityFetcher,
