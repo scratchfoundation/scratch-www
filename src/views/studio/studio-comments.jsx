9c83271af70e438b33de4dec31043409eee7e608
Karishma Chadha <kchadha@media.mit.edu>
2021-03-31 14:41:49 -0400

    Create studio comment action creators. Add comments reducer and initial studio comment actions to studio comments view. Display preliminary data fetching.

--- src/views/studio/studio-comments.jsx
+++ src/views/studio/studio-comments.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
 import {useParams} from 'react-router-dom';
+import {connect} from 'react-redux';
+import Debug from './debug.jsx';
 
-const StudioComments = () => {
+import studioCommentActions from '../../redux/studio-comment-actions.js';
+
+const StudioComments = props => {
     const {studioId} = useParams();
-
+    useEffect(() => {
+        if (props.comments.length === 0) props.getTopLevelComments(studioId, 0)
+    }, [studioId]);
     return (
         <div>
             <h2>Comments</h2>
+            <Debug
+                label="Comments"
+                data={props}
+            />
             <p>Studio {studioId}</p>
         </div>
     );
 };
 
-export default StudioComments;
+StudioComments.propTypes = {
+    comments: PropTypes.arrayOf(PropTypes.shape({})),
+    getTopLevelComments: PropTypes.func
+};
+
+
+export default connect(
+    state => state.comments,
+    {
+        getTopLevelComments: studioCommentActions.getTopLevelComments
+    }
+)(StudioComments);
