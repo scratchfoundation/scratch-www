06a95138047610c7724378d0dedf5742b3330d08
Benjamin Wheeler <wheeler.benjamin@gmail.com>
2021-05-05 16:33:10 -0400

    Revert "Revert "Add privacy policy and terms of use to allowlist""

--- src/redux/session.js
+++ src/redux/session.js
@@ -13,9 +13,10 @@ const Types = keyMirror({
 });
 
 const banWhitelistPaths = [
-    '/accounts/banned-response/',
-    '/community_guidelines/',
-    '/community_guidelines'
+    '/accounts/banned-response',
+    '/community_guidelines',
+    '/privacy_policy',
+    '/terms_of_use'
 ];
 
 module.exports.Status = keyMirror({
