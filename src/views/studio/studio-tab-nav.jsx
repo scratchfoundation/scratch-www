27bdabcfeb7f5cdae0ee5d88b3007d9fb9ee064f
Paul Kaplan <pkaplan@media.mit.edu>
2021-05-06 09:02:05 -0400

    Fix trailing slash breaking tab navigation

--- src/views/studio/studio-tab-nav.jsx
+++ src/views/studio/studio-tab-nav.jsx
@@ -3,8 +3,8 @@ import {useRouteMatch, NavLink} from 'react-router-dom';
 import SubNavigation from '../../components/subnavigation/subnavigation.jsx';
 
 const StudioTabNav = () => {
-    const match = useRouteMatch();
-
+    const {params: {studioPath, studioId}} = useRouteMatch();
+    const base = `/${studioPath}/${studioId}`;
     return (
         <SubNavigation
             align="left"
@@ -12,26 +12,26 @@ const StudioTabNav = () => {
         >
             <NavLink
                 activeClassName="active"
-                to={`${match.url}`}
+                to={base}
                 exact
             >
                 <li>Projects</li>
             </NavLink>
             <NavLink
                 activeClassName="active"
-                to={`${match.url}/curators`}
+                to={`${base}/curators`}
             >
                 <li>Curators</li>
             </NavLink>
             <NavLink
                 activeClassName="active"
-                to={`${match.url}/comments`}
+                to={`${base}/comments`}
             >
                 <li> Comments</li>
             </NavLink>
             <NavLink
                 activeClassName="active"
-                to={`${match.url}/activity`}
+                to={`${base}/activity`}
             >
                 <li>Activity</li>
             </NavLink>
