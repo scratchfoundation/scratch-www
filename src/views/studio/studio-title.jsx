1d728069146824b004fa205d257a4838bed453d1
Paul Kaplan <pkaplan@media.mit.edu>
2021-05-19 15:36:56 -0400

    Also use a div for the title so it doesn't use scrollbars and fix the alignment

--- src/views/studio/studio-title.jsx
+++ src/views/studio/studio-title.jsx
@@ -29,18 +29,24 @@ const StudioTitle = ({
     });
     return (
         <div className="studio-info-section">
-            <textarea
-                className={fieldClassName}
-                disabled={isMutating || !canEditInfo || isFetching}
-                defaultValue={title}
-                onKeyDown={e => e.key === 'Enter' && e.target.blur()}
-                onBlur={e => e.target.value !== title &&
-                    handleUpdate(e.target.value)}
-            />
-            {titleError && <ValidationMessage
-                mode="error"
-                message={<FormattedMessage id={errorToMessageId(titleError)} />}
-            />}
+            {canEditInfo ? (
+                <React.Fragment>
+                    <textarea
+                        className={fieldClassName}
+                        disabled={isMutating || !canEditInfo || isFetching}
+                        defaultValue={title}
+                        onKeyDown={e => e.key === 'Enter' && e.target.blur()}
+                        onBlur={e => e.target.value !== title &&
+                            handleUpdate(e.target.value)}
+                    />
+                    {titleError && <ValidationMessage
+                        mode="error"
+                        message={<FormattedMessage id={errorToMessageId(titleError)} />}
+                    />}
+                </React.Fragment>
+            ) : (
+                <div className={fieldClassName}>{title}</div>
+            )}
         </div>
     );
 };
