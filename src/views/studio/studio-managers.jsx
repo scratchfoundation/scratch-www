96aeabd255955d002c4e8019e21cf051275d0b1d
Karishma Chadha <kchadha@media.mit.edu>
2021-06-10 19:24:25 -0400

    Display info to managers/studio creator about how close the studio is to reaching the manager limit when over a threshold. Display info about removing managers when a studio has reached the limit.

--- src/views/studio/studio-managers.jsx
+++ src/views/studio/studio-managers.jsx
@@ -1,28 +1,89 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {FormattedMessage} from 'react-intl';
 import classNames from 'classnames';
 
 import {managers} from './lib/redux-modules';
+import {
+    STUDIO_MANAGER_LIMIT,
+    selectStudioManagerCount,
+    selectStudioHasReachedManagerLimit,
+    selectStudioHasReachedManagerThreshold
+} from '../../redux/studio.js';
 import {loadManagers} from './lib/studio-member-actions';
 import Debug from './debug.jsx';
 import {ManagerTile} from './studio-member-tile.jsx';
+import StudioInfoBox from './studio-info-box.jsx';
 import AlertProvider from '../../components/alert/alert-provider.jsx';
 import Alert from '../../components/alert/alert.jsx';
+import {
+    selectCanRemoveManager, selectCanPromoteCurators
+} from '../../redux/studio-permissions';
 
 
-const StudioManagers = ({items, error, loading, moreToLoad, onLoadMore}) => {
+const StudioManagers = ({
+    canPromoteCurators,
+    canRemoveManagers,
+    managerCount,
+    hasReachedManagerLimit,
+    hasReachedManagerThreshold,
+    items,
+    error,
+    loading,
+    moreToLoad,
+    onLoadMore
+}) => {
     useEffect(() => {
         if (items.length === 0) onLoadMore();
     }, []);
+
+    const [infoBoxDismissed, setInfoBoxDismissed] = useState(false);
+
+    const showManagerLimitInfoBox =
+        !infoBoxDismissed && canPromoteCurators &&
+        canRemoveManagers && hasReachedManagerLimit;
 
     return (
         <AlertProvider>
             <div className="studio-members">
                 <Alert className="studio-alert" />
-                <div className="studio-header-container">
+                <StudioInfoBox
+                    showInfoBox={showManagerLimitInfoBox}
+                    // eslint-disable-next-line react/jsx-no-bind
+                    onClose={() => setInfoBoxDismissed(true)}
+                >
+                    <div className="manager-threshold-message">
+                        <span className="manager-threshold-info">
+                            <FormattedMessage
+                                id="studio.managerThresholdInfo"
+                                values={{
+                                    numberOfManagers: managerCount,
+                                    managerLimit: STUDIO_MANAGER_LIMIT
+                                }}
+                            />
+                        </span>
+                        <FormattedMessage
+                            id="studio.managerThresholdRemoveManagers"
+                            values={{
+                                managerLimit: STUDIO_MANAGER_LIMIT
+                            }} 
+                        />
+                    </div>
+                </StudioInfoBox>
+                <div className="studio-header-container studio-managers-header">
                     <h2><FormattedMessage id="studio.managersHeader" /></h2>
+                    {canPromoteCurators && canRemoveManagers && hasReachedManagerThreshold &&
+                        <div className="studio-manager-count">
+                            <FormattedMessage
+                                id="studio.managerCountInfo"
+                                values={{
+                                    numberOfManagers: managerCount,
+                                    managerLimit: STUDIO_MANAGER_LIMIT
+                                }}
+                            />
+                        </div>
+                    }
                 </div>
                 {error && <Debug
                     label="Error"
@@ -56,6 +117,11 @@ const StudioManagers = ({items, error, loading, moreToLoad, onLoadMore}) => {
 };
 
 StudioManagers.propTypes = {
+    canPromoteCurators: PropTypes.bool,
+    canRemoveManagers: PropTypes.bool,
+    managerCount: PropTypes.number,
+    hasReachedManagerLimit: PropTypes.bool,
+    hasReachedManagerThreshold: PropTypes.bool,
     items: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.id,
         username: PropTypes.string,
@@ -72,7 +138,14 @@ StudioManagers.propTypes = {
 };
 
 export default connect(
-    state => managers.selector(state),
+    state => ({
+        canPromoteCurators: selectCanPromoteCurators(state),
+        canRemoveManagers: selectCanRemoveManager(state),
+        managerCount: selectStudioManagerCount(state),
+        hasReachedManagerLimit: selectStudioHasReachedManagerLimit(state),
+        hasReachedManagerThreshold: selectStudioHasReachedManagerThreshold(state),
+        ...managers.selector(state)
+    }),
     {
         onLoadMore: loadManagers
     }
