ac42bc3a9110e3f5cd0232ccfaeec12febab532c
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2021-06-24 17:14:01 -0400

    Use `hidden` prop from `../www/footer.jsx`

--- src/components/footer/container/footer.jsx
+++ src/components/footer/container/footer.jsx
@@ -1,16 +1,21 @@
 const PropTypes = require('prop-types');
 const React = require('react');
 
+const classNames = require('classnames');
+
 require('./footer.scss');
 
 const FooterBox = props => (
-    <div className="inner">
+    <div className={classNames("inner", {
+        hidden: props.hidden
+    })}>
         {props.children}
     </div>
 );
 
 FooterBox.propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    hidden: PropTypes.bool
 };
 
 module.exports = FooterBox;
