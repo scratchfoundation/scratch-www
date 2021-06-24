975d24cd5d627219d382f2666cda2604c0e548bb
Paul Kaplan <pkaplan@media.mit.edu>
2021-06-24 10:59:09 -0400

    Revert "Revert "Only request replies if reply_count > 0""

--- src/redux/studio-comment-actions.js
+++ src/redux/studio-comment-actions.js
@@ -90,7 +90,10 @@ const getTopLevelComments = () => ((dispatch, getState) => {
         }
         dispatch(setFetchStatus('comments', Status.FETCHED));
         dispatch(setComments(body));
-        dispatch(getReplies(body.map(comment => comment.id), 0));
+        const commentsWithReplies = body.filter(comment => comment.reply_count > 0);
+        if (commentsWithReplies.length > 0) {
+            dispatch(getReplies(commentsWithReplies.map(comment => comment.id), 0));
+        }
 
         // If we loaded a full page of comments, assume there are more to load.
         // This will be wrong (1 / COMMENT_LIMIT) of the time, but does not require
@@ -130,7 +133,9 @@ const getCommentById = commentId => ((dispatch, getState) => {
         // If the comment is not a reply, show it as top level and load replies
         dispatch(setFetchStatus('comments', Status.FETCHED));
         dispatch(setComments([body]));
-        dispatch(getReplies(body.id, 0));
+        if (body.reply_count > 0) {
+            dispatch(getReplies(body.id, 0));
+        }
     });
 });
 
