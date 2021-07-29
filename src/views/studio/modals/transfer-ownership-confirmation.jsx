2c7d6b4cd0ac1a2bb9c74a6bbea464536fcc60f1
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2021-07-29 10:37:21 -0400

    use arrow svg

--- src/views/studio/modals/transfer-ownership-confirmation.jsx
+++ src/views/studio/modals/transfer-ownership-confirmation.jsx
@@ -38,7 +38,9 @@ const TransferOwnershipConfirmation = ({
                     username={currentOwnerUsername}
                     isCreator={false}
                 />
-                <span>➡️</span>
+                <img
+                    src="/svgs/studio/r-arrow.svg"
+                />
                 <TransferOwnershipTile
                     key={selectedId}
                     id={selectedId}
