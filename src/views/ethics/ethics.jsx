df46afbcf54c926528695452186d13fb3d52c9f2
seotts <sotts@scratch.mit.edu>
2021-10-12 16:27:52 -0400

    Add code of ethics page

---
+++ src/views/ethics/ethics.jsx
@@ -0,0 +1,59 @@
+const React = require('react');
+const FormattedMessage = require('react-intl').FormattedMessage;
+
+const Page = require('../../components/page/www/page.jsx');
+const render = require('../../lib/render.jsx');
+
+const InformationPage = require('../../components/informationpage/informationpage.jsx');
+
+const Ethics = () => (
+    <InformationPage
+        title={
+            <FormattedMessage id="ethics.title" />
+        }
+    >
+        <div className="inner info-inner">
+            <span className="nav-spacer" />
+
+            <p><FormattedMessage id="ethics.intro1" /></p>
+            <p><FormattedMessage id="ethics.intro2" /></p>
+
+            <p><FormattedMessage
+                id="ethics.principle1"
+                values={{
+                    title: (
+                        <strong>
+                            <FormattedMessage id="ethics.principle1Title" />
+                        </strong>
+                    )
+                }}
+            /></p>
+
+            <p><FormattedMessage
+                id="ethics.principle2"
+                values={{
+                    title: (
+                        <strong>
+                            <FormattedMessage id="ethics.principle2Title" />
+                        </strong>
+                    )
+                }}
+            /></p>
+
+            <p><FormattedMessage
+                id="ethics.principle3"
+                values={{
+                    title: (
+                        <strong>
+                            <FormattedMessage id="ethics.principle3Title" />
+                        </strong>
+                    )
+                }}
+            /></p>
+
+            <span className="nav-spacer" />
+        </div>
+    </InformationPage>
+);
+
+render(<Page><Ethics /></Page>, document.getElementById('app'));
