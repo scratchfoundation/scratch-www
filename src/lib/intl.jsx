c2f0697eed034cdf06dda03b136931dfb8af1af6
rschamp <ray@scratch.mit.edu>
2021-11-15 11:40:19 -0500

    Remove addLocaleData, it's supplied with a polyfill
    https://formatjs.io/docs/react-intl/upgrade-guide-3x/#migrate-to-using-native-intl-apis

--- src/lib/intl.jsx
+++ src/lib/intl.jsx
@@ -1,7 +1,3 @@
 const ReactIntl = require('react-intl');
 
-// Add locale data to react intl for all supported languages
-const localeData = require('scratch-l10n').localeData;
-ReactIntl.addLocaleData(localeData);
-
 module.exports = ReactIntl;
