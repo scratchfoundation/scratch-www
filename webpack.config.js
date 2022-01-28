5b2d5704dac1e0b0587435fe7389ed6225f4176c
BryceLTaylor <brycet@media.mit.edu>
2022-01-28 10:17:13 -0500

    Update autoprefixer to 9.8.6

--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,7 @@ module.exports = {
                         loader: 'postcss-loader',
                         options: {
                             plugins: function () {
-                                return [autoprefixer({browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']})];
+                                return [autoprefixer()];
                             }
                         }
                     },
@@ -117,7 +117,7 @@ module.exports = {
                         loader: 'postcss-loader',
                         options: {
                             plugins: function () {
-                                return [autoprefixer({browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']})];
+                                return [autoprefixer()];
                             }
                         }
                     }
