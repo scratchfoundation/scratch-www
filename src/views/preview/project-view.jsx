16ae1a5d0f3e293a2e3856b59a1c7dcf42867831
tomlum <tomlumperson@gmail.com>
2022-05-24 14:06:41 -0400

    adds check for newly created project to not require token

--- src/views/preview/project-view.jsx
+++ src/views/preview/project-view.jsx
@@ -833,7 +833,7 @@ class Preview extends React.Component {
                         />
                     </Page> :
                     <React.Fragment>
-                        {this.props.projectInfo && this.props.projectInfo.project_token && (
+                        {(parseInt(this.state.projectId, 10) === 0 || (this.props.projectInfo && this.props.projectInfo.project_token)) && (
                             <IntlGUI
                                 assetHost={this.props.assetHost}
                                 authorId={this.props.authorId}
