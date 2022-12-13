4d6b5bd2e9f225f6c56113bacf8ed3997ac5cdc0
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2022-12-13 08:31:56 -0500

    Update tab item

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -1,30 +1,51 @@
 const classNames = require('classnames');
 const PropTypes = require('prop-types');
+const { useRef } = require('react');
 const React = require('react');
 
 const SubNavigation = require('../../components/subnavigation/subnavigation.jsx');
 
 require('./tabs.scss');
 
+const TabItem = ({ children, ...props }) => {
+    const tabItemRef = useRef()
+    
+    return <li ref={tabItemRef} {...props}>{children}</li>
+}
+
 /*
  * Container for a custom, horizontal list of navigation elements
  * that can be displayed within a view or component.
  */
 const Tabs = ({ items, activeTabName }) => {
-    const itemsRendered = items.map(({ name, onTrigger, getContent }) => {
+    let activeIndex
+    const itemsRendered = items.map(({ name, onTrigger, getContent }, index) => {
         const isActive = name === activeTabName
+        activeIndex = index
         return (
             <li 
                 className={`${isActive ? 'active' : ''}`}
                 onClick={onTrigger}
+                tabIndex={isActive ? 0 : -1}
                 key={name}
             >
                 {getContent(isActive)}
             </li>
         )
     })
+
+    const handleKeyDown = event => {
+        if (!['ArrowLeft', 'ArrowRight', 'Home', 'End', 'Enter', ' '].includes(event.key)) {
+            return
+        }
+        event.preventDefault()
+        console.log(
+            'hi'
+        )
+    }
+
     return (
-        <div className="tab-background">
+        <div className="tab-background" onKeyDown={handleKeyDown}>
             <SubNavigation className="tabs">
                 {itemsRendered}
             </SubNavigation>
