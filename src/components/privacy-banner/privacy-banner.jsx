8220220af1067d21a80e5e1daaafa59a162fa300
cori hudson <cori@scratch.mit.edu>
2023-05-03 16:13:40 -0400

    feat(privacy banner): set expiration of policy cookie to 3 weeks

--- src/components/privacy-banner/privacy-banner.jsx
+++ src/components/privacy-banner/privacy-banner.jsx
@@ -34,13 +34,15 @@ class PrivacyBanner extends React.Component {
     }
 
     handleCloseBanner () {
-        const opts = {};
+        const opts = {
+            expires: new Date(new Date().setDate(new Date().getDate + 21)) // expires after 3 weeks
+        };
         this.setState({dismissedPrivacyBanner: true});
         jar.set('scratchpolicyseen', true, opts);
     }
     render () {
         const showBanner = this.shouldShowBanner();
-        const privacyPolicyLink = chunks => <a href="/privacy_policy">{chunks}</a>
+        const privacyPolicyLink = chunks => <a href="/privacy_policy">{chunks}</a>;
         if (showBanner) {
             return (
                 <aside>
