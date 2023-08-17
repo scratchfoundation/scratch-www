c72f9bef4dc44477615c29079a9df40f4b106c33
GrahamSH <grahamshllk@gmail.com>
2023-08-17 14:46:27 -0400

    Update to scratch.org domains
    🙃

--- webpack.config.js
+++ webpack.config.js
@@ -207,13 +207,13 @@ module.exports = {
         }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"' + (process.env.NODE_ENV || 'development') + '"',
-            'process.env.API_HOST': '"' + (process.env.API_HOST || 'https://api.scratch.mit.edu') + '"',
+            'process.env.API_HOST': '"' + (process.env.API_HOST || 'https://scratch-api.scratch.org') + '"',
             'process.env.RECAPTCHA_SITE_KEY': '"' +
                     (process.env.RECAPTCHA_SITE_KEY || '6Lf6kK4UAAAAABKTyvdSqgcSVASEnMrCquiAkjVW') + '"',
-            'process.env.ASSET_HOST': '"' + (process.env.ASSET_HOST || 'https://assets.scratch.mit.edu') + '"',
+            'process.env.ASSET_HOST': '"' + (process.env.ASSET_HOST || 'https://scratch-assets.scratch.org') + '"',
             'process.env.BACKPACK_HOST': '"' + (process.env.BACKPACK_HOST || 'https://backpack.scratch.mit.edu') + '"',
             'process.env.CLOUDDATA_HOST': '"' + (process.env.CLOUDDATA_HOST || 'clouddata.scratch.mit.edu') + '"',
-            'process.env.PROJECT_HOST': '"' + (process.env.PROJECT_HOST || 'https://projects.scratch.mit.edu') + '"',
+            'process.env.PROJECT_HOST': '"' + (process.env.PROJECT_HOST || 'https://scratch-projects.scratch.org') + '"',
             'process.env.STATIC_HOST': '"' + (process.env.STATIC_HOST || 'https://uploads.scratch.mit.edu') + '"',
             'process.env.SCRATCH_ENV': '"' + (process.env.SCRATCH_ENV || 'development') + '"'
         })
