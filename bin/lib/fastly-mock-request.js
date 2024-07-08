8f5ef758a1ead2121aa76e73760c889de7af107c
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2024-07-08 12:52:19 -0700

    chore: persist mock Fastly state for debugging

--- bin/lib/fastly-mock-request.js
+++ bin/lib/fastly-mock-request.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
 const mockServiceId = 'MOCK';
 
 const HTTP_METHOD = {
@@ -28,6 +32,111 @@ const HTTP_METHOD = {
  * @property {object} params Request parameters
  * @property {RegExpMatchArray} match Results from the URL match
  */
+
+/**
+ * Directory to persist Fastly data to
+ * @type {string}
+ * @private
+ */
+let persistDir = null;
+
+/**
+ * State of the mocked Fastly API.
+ */
+const fastlyMockState = {
+    conditions: {},
+    headers: {},
+    responseObjects: {},
+    vcl: {}
+};
+
+/**
+ * @returns {string} A timestamp in a format suitable for logging
+ */
+const makeLogTimestamp = () =>
+    new Date()
+        .toLocaleString('sv'); // cheat: Swedish locale uses ISO 8601 format
+
+/**
+ * When util.inspect sorts object keys, it calls its comparison function with strings.
+ * The strings represent object keys, like 'name: "value"'.
+ * When we're trying to sort conditions or headers, it'd be nice to sort them by priority.
+ * In those cases, the comparison string will be like 'someName: {priority: 10}'.
+ * Note that Fastly processes these items in order of increasing priority ("Lower numbers execute first").
+ * This function extracts the priority from the comparison string.
+ * @param {string} compareString The comparison string to parse, e.g. 'name: "value"'
+ * @returns {number} The parsed priority, or undefined if no priority could be retrieved.
+ */
+const getPriority = compareString => {
+    if (getPriority.memos.has(compareString)) {
+        return getPriority.memos.get(compareString);
+    }
+    try {
+        const obj = new Function(`return {${compareString}}`)();
+        for (const v of Object.values(obj)) {
+            if (typeof v.priority !== 'undefined') {
+                const priority = Number(v.priority);
+                getPriority.memos.set(compareString, priority);
+                return priority;
+            }
+        }
+    } catch (e) {
+        // ignore
+    }
+};
+
+getPriority.memos = new Map();
+
+/**
+ * Write the current state of the mocked Fastly API to disk
+ */
+const writeState = () => {
+    if (persistDir) {
+        const {vcl, ...rest} = fastlyMockState;
+        const stateString = util.inspect(rest, {
+            depth: Infinity,
+            colors: false,
+            maxArrayLength: Infinity,
+            maxStringLength: Infinity,
+            breakLength: 78,
+            // compact: true,
+            numericSeparator: true,
+            sorted: (aString, bString) => {
+                let aPriority = getPriority(aString);
+                let bPriority = getPriority(bString);
+
+                if (aPriority !== bPriority) {
+                    // At least one of them specifies a priority, but maybe not both.
+                    if (typeof aPriority === 'undefined') {
+                        aPriority = 100; // Fastly default priority
+                    }
+                    if (typeof bPriority === 'undefined') {
+                        bPriority = 100; // Fastly default priority
+                    }
+                    return aPriority - bPriority;
+                }
+
+                if (aString < bString) {
+                    return -1;
+                }
+                if (aString > bString) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+        fs.writeFileSync(
+            path.join(persistDir, 'fastly-mock-state.js'),
+            `/* eslint-disable indent */\nmodule.exports = ${stateString};\n`
+        );
+        for (const [name, content] of Object.entries(vcl)) {
+            fs.writeFileSync(
+                path.join(persistDir, `fastly-mock-vcl-${name}.vcl`),
+                content
+            );
+        }
+    }
+};
 
 /**
  * Ordered list of mocked requests and their handlers.
@@ -97,46 +206,90 @@ const mockedRequests = [
         name: 'vcl-condition',
         match: new RegExp(`^/service/${mockServiceId}/version/\\d+/condition/`),
         handler: ({method, params}) => {
-            if (method === HTTP_METHOD.PUT) {
+            switch (method) {
+            case HTTP_METHOD.POST:
+                console.log(`Mocking creation of condition: ${params.name}`);
+                break;
+            case HTTP_METHOD.PUT:
                 console.log(`Mocking update for condition: ${params.name}`);
-                return {
-                    name: params.name
-                };
+                break;
+            case HTTP_METHOD.GET:
+            default:
+                throw new Error('Method not mocked');
             }
-            throw new Error('Method not mocked');
+
+            const {name: _, ...outputParams} = params;
+            fastlyMockState.conditions[params.name] = outputParams;
+
+            return {
+                name: params.name
+            };
         }
     },
     {
         name: 'vcl-header',
         match: new RegExp(`^/service/${mockServiceId}/version/\\d+/header/`),
         handler: ({method, params}) => {
-            if (method === HTTP_METHOD.PUT) {
+            switch (method) {
+            case HTTP_METHOD.POST:
+                console.log(`Mocking creation of header: ${params.name}`);
+                break;
+            case HTTP_METHOD.PUT:
                 console.log(`Mocking update for header: ${params.name}`);
-                return;
+                break;
+            case HTTP_METHOD.GET:
+            default:
+                throw new Error('Method not mocked');
             }
-            throw new Error('Method not mocked');
+
+            const {name: _, ...outputParams} = params;
+            fastlyMockState.headers[params.name] = outputParams;
+
+            // return {???};
         }
     },
     {
         name: 'vcl-response-object',
         match: new RegExp(`^/service/${mockServiceId}/version/\\d+/response_object/`),
         handler: ({method, params}) => {
-            if (method === HTTP_METHOD.PUT) {
+            switch (method) {
+            case HTTP_METHOD.POST:
+                console.log(`Mocking creation of response object: ${params.name}`);
+                break;
+            case HTTP_METHOD.PUT:
                 console.log(`Mocking update for response object: ${params.name}`);
-                return;
+                break;
+            case HTTP_METHOD.GET:
+            default:
+                throw new Error('Method not mocked');
             }
-            throw new Error('Method not mocked');
+
+            const {name: _, ...outputParams} = params;
+            fastlyMockState.responseObjects[params.name] = outputParams;
+
+            // return {???};
         }
     },
     {
         name: 'vcl-vcl-custom',
         match: new RegExp(`^/service/${mockServiceId}/version/\\d+/vcl/(.*)$`),
-        handler: ({match, method}) => {
-            if (method === HTTP_METHOD.PUT) {
-                console.log(`Mocking update for custom VCL: ${match[1]}`);
-                return;
+        handler: ({match, method, params}) => {
+            const name = match[1];
+            switch (method) {
+            case HTTP_METHOD.POST:
+                console.log(`Mocking creation of VCL: ${name}`);
+                break;
+            case HTTP_METHOD.PUT:
+                console.log(`Mocking update for VCL: ${name}`);
+                break;
+            case HTTP_METHOD.GET:
+            default:
+                throw new Error('Method not mocked');
             }
-            throw new Error('Method not mocked');
+
+            fastlyMockState.vcl[name] = params.content;
+
+            // return {???};
         }
     }
 ];
@@ -161,7 +314,14 @@ const fastlyMockRequest = (method, url, params, callback) => {
         callback = params;
         params = null;
     }
-    // console.log('Simulating Fastly request:', {method, url, params});
+
+    if (persistDir) {
+        fs.writeFileSync(
+            path.join(persistDir, 'fastly-mock-requests.log'),
+            `${makeLogTimestamp()} ${method} ${url}\n`,
+            {flag: 'a'}
+        );
+    }
 
     let handler;
     let match;
@@ -188,7 +348,23 @@ const fastlyMockRequest = (method, url, params, callback) => {
     callback(err, res);
 };
 
+/**
+ * Persist data from mocked Fastly requests to disk
+ * @param {string} outputDir The directory to persist Fastly data to. Will be created if it does not exist.
+ */
+const fastlyMockPersist = outputDir => {
+    console.log('Persisting Fastly data to:', outputDir);
+    fs.mkdirSync(outputDir, {recursive: true});
+    persistDir = outputDir;
+    process.on('beforeExit', () => {
+        writeState();
+    });
+};
+
+// fastlyMockPersist('./fastlyMock');
+
 module.exports = {
+    fastlyMockPersist,
     fastlyMockRequest,
     mockServiceId
 };
