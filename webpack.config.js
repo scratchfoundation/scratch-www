0c2e42c238ee9090437a83325bf3bb50cbd69e98
MiroslavDionisiev <miroslav@scratch.org>
2024-10-23 16:25:03 +0300

    chore: removed unused service

--- webpack.config.js
+++ webpack.config.js
@@ -285,8 +285,6 @@ module.exports = {
             'process.env.GA_ID': `"${process.env.GA_ID || 'UA-000000-01'}"`,
             'process.env.GTM_ENV_AUTH': `"${process.env.GTM_ENV_AUTH || ''}"`,
             'process.env.GTM_ID': process.env.GTM_ID ? `"${process.env.GTM_ID}"` : null,
-            'process.env.USER_GUIDING_ID': `"${process.env.USER_GUIDING_ID || ''}"`,
-            'process.env.SORTING_HAT_HOST': `"${process.env.SORTING_HAT_HOST || ''}"`,
             'process.env.ONBOARDING_TESTING_TOGGLED': `"${
                 process.env.ONBOARDING_TESTING_TOGGLED || true
             }"`,
