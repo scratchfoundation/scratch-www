e137357fb7a4e638614a6a33d7e69ef3258ab10f
MiroslavDionisiev <miroslav@scratch.org>
2024-11-06 11:19:50 +0200

    chore: fixed merge error

--- webpack.config.js
+++ webpack.config.js
@@ -285,8 +285,8 @@ module.exports = {
             'process.env.GA_ID': `"${process.env.GA_ID || 'UA-000000-01'}"`,
             'process.env.GTM_ENV_AUTH': `"${process.env.GTM_ENV_AUTH || ''}"`,
             'process.env.GTM_ID': process.env.GTM_ID ? `"${process.env.GTM_ID}"` : null,
-            'process.env.ONBOARDING_TESTING_TOGGLED': `"${
-                process.env.ONBOARDING_TESTING_TOGGLED || true
+            'process.env.ONBOARDING_TEST_ACTIVE': `"${
+                process.env.ONBOARDING_TEST_ACTIVE || true
             }"`,
             'process.env.ONBOARDING_TESTING_STARTING_DATE': `"${
                 process.env.ONBOARDING_TESTING_STARTING_DATE || '2024-01-20'
