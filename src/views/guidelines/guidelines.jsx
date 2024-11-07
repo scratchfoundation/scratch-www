6eda19637e6ed35423c04354dead7af40d2e21ad
MiroslavDionisiev <miroslav@scratch.org>
2024-11-07 14:09:24 +0200

    feat: [UEPR-67] Added alternative texts for images on guidelines page

--- src/views/guidelines/guidelines.jsx
+++ src/views/guidelines/guidelines.jsx
@@ -3,6 +3,7 @@ const React = require('react');
 
 const Page = require('../../components/page/www/page.jsx');
 const render = require('../../lib/render.jsx');
+const {useIntl} = require('react-intl');
 
 require('./guidelines.scss');
 
@@ -15,7 +16,9 @@ const pageSections = [
             {id: 'guidelines.respectbody2'}
         ],
         sectionImgSrc: '/svgs/guidelines/illustration_respect.svg',
-        buttonImgSrc: '/svgs/guidelines/blobblue_respect.svg'
+        sectionImgAltTextId: 'guidelines.respectSectionImageDescription',
+        buttonImgSrc: '/svgs/guidelines/blobblue_respect.svg',
+        buttonImgAltTextId: 'guidelines.respectButtonImageDescription'
     },
     {
         id: 'privacy',
@@ -25,7 +28,9 @@ const pageSections = [
             {id: 'guidelines.privacybody2'}
         ],
         sectionImgSrc: '/svgs/guidelines/illustration_safe.svg',
-        buttonImgSrc: '/svgs/guidelines/blobyellow_safe.svg'
+        sectionImgAltTextId: 'guidelines.privacySectionImageDescription',
+        buttonImgSrc: '/svgs/guidelines/blobyellow_safe.svg',
+        buttonImgAltTextId: 'guidelines.privacyButtonImageDescription'
     },
     {
         id: 'helpful',
@@ -35,7 +40,9 @@ const pageSections = [
             {id: 'guidelines.helpfulbody2'}
         ],
         sectionImgSrc: '/svgs/guidelines/illustration_feedback.svg',
-        buttonImgSrc: '/svgs/guidelines/blobmagenta_feedback.svg'
+        sectionImgAltTextId: 'guidelines.helpfulSectionImageDescription',
+        buttonImgSrc: '/svgs/guidelines/blobmagenta_feedback.svg',
+        buttonImgAltTextId: 'guidelines.helpfulButtonImageDescription'
     },
     {
         id: 'remix',
@@ -45,7 +52,9 @@ const pageSections = [
             {id: 'guidelines.remixbody2'}
         ],
         sectionImgSrc: '/svgs/guidelines/illustration_remix.svg',
-        buttonImgSrc: '/svgs/guidelines/blobgreen_remix.svg'
+        sectionImgAltTextId: 'guidelines.remixSectionImageDescription',
+        buttonImgSrc: '/svgs/guidelines/blobgreen_remix.svg',
+        buttonImgAltTextId: 'guidelines.remixButtonImageDescription'
     },
     {
         id: 'honesty',
@@ -55,7 +64,9 @@ const pageSections = [
             {id: 'guidelines.honestybody2'}
         ],
         sectionImgSrc: '/svgs/guidelines/illustration_honest.svg',
-        buttonImgSrc: '/svgs/guidelines/blobpurple_honest.svg'
+        sectionImgAltTextId: 'guidelines.honestySectionImageDescription',
+        buttonImgSrc: '/svgs/guidelines/blobpurple_honest.svg',
+        buttonImgAltTextId: 'guidelines.honestyButtonImageDescription'
     },
     {
         id: 'friendly',
@@ -65,7 +76,9 @@ const pageSections = [
             {id: 'guidelines.friendlybody2'}
         ],
         sectionImgSrc: '/svgs/guidelines/illustration_friendly.svg',
-        buttonImgSrc: '/svgs/guidelines/blobpink_friendly.svg'
+        sectionImgAltTextId: 'guidelines.friendlySectionImageDescription',
+        buttonImgSrc: '/svgs/guidelines/blobpink_friendly.svg',
+        buttonImgAltTextId: 'guidelines.friendlyButtonImageDescription'
     },
     {
         id: 'learn-more',
@@ -83,69 +96,80 @@ const pageSections = [
             },
             {id: 'guidelines.learnMorebody2'}
         ],
-        sectionImgSrc: '/svgs/guidelines/illustration_learn_more.svg'
+        sectionImgSrc: '/svgs/guidelines/illustration_learn_more.svg',
+        sectionImgAltTextId: 'guidelines.learnMoreSectionImageDescription'
     }
 ];
 
-const Guidelines = () => (
-    <div className="guidelines-page">
-        <header>
-            <div className="title">
-                <FormattedMessage id="guidelines.title" />
-            </div>
-            <div className="header1">
-                <FormattedMessage id="guidelines.header1" />
-            </div>
-            <div className="header2">
-                <FormattedMessage id="guidelines.header2" />
-            </div>
-        </header>
-        <section className="navigation">
-            <div className="header3">
-                <FormattedMessage id="guidelines.header3" />
-            </div>
-            <section className="navigation-buttons">
-                {pageSections
-                    .filter(guide => guide.buttonImgSrc)
-                    .map(({id, headerTextId, buttonImgSrc}) => (
-                        <a
-                            key={id}
-                            href={`#${id}`}
-                        >
-                            <img src={buttonImgSrc} />
-                            <FormattedMessage id={headerTextId} />
-                        </a>
-                    ))}
-            </section>
-        </section>
-        <section className="inner guidelines">
-            {pageSections.map(
-                ({id, headerTextId, contentTexts, sectionImgSrc}, index) => (
-                    <div
-                        id={id}
-                        key={id}
-                        className={`guideline ${
-                            index % 2 === 0 ? 'content-first' : 'image-first'
-                        }`}
-                    >
-                        <div>
-                            <div className="guideline-title">
+const Guidelines = () => {
+    const intl = useIntl();
+
+    return (
+        <div className="guidelines-page">
+            <header>
+                <div className="title">
+                    <FormattedMessage id="guidelines.title" />
+                </div>
+                <div className="header1">
+                    <FormattedMessage id="guidelines.header1" />
+                </div>
+                <div className="header2">
+                    <FormattedMessage id="guidelines.header2" />
+                </div>
+            </header>
+            <section className="navigation">
+                <div className="header3">
+                    <FormattedMessage id="guidelines.header3" />
+                </div>
+                <section className="navigation-buttons">
+                    {pageSections
+                        .filter(guide => guide.buttonImgSrc)
+                        .map(({id, headerTextId, buttonImgSrc, buttonImgAltTextId}) => (
+                            <a
+                                key={id}
+                                href={`#${id}`}
+                            >
+                                <img
+                                    alt={intl.formatMessage({id: buttonImgAltTextId})}
+                                    src={buttonImgSrc}
+                                />
                                 <FormattedMessage id={headerTextId} />
+                            </a>
+                        ))}
+                </section>
+            </section>
+            <section className="inner guidelines">
+                {pageSections.map(
+                    ({id, headerTextId, contentTexts, sectionImgSrc, sectionImgAltTextId}, index) => (
+                        <div
+                            id={id}
+                            key={id}
+                            className={`guideline ${
+                                index % 2 === 0 ? 'content-first' : 'image-first'
+                            }`}
+                        >
+                            <div>
+                                <div className="guideline-title">
+                                    <FormattedMessage id={headerTextId} />
+                                </div>
+                                <p className="first-paragraph">
+                                    <FormattedMessage {...contentTexts[0]} />
+                                </p>
+                                <p className="second-paragraph">
+                                    <FormattedMessage {...contentTexts[1]} />
+                                </p>
                             </div>
-                            <p className="first-paragraph">
-                                <FormattedMessage {...contentTexts[0]} />
-                            </p>
-                            <p className="second-paragraph">
-                                <FormattedMessage {...contentTexts[1]} />
-                            </p>
+                            <img
+                                alt={intl.formatMessage({id: sectionImgAltTextId})}
+                                src={sectionImgSrc}
+                            />
                         </div>
-                        <img src={sectionImgSrc} />
-                    </div>
-                )
-            )}
-        </section>
-    </div>
-);
+                    )
+                )}
+            </section>
+        </div>
+    );
+};
 
 render(
     <Page>
