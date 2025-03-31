26257f0b8308df0293e8489150f6d61496f5bef4
William Dickerson <william@scratch.org>
2025-03-31 13:06:08 -0400

    feat[POD-182]: Update text of donation banner
    Update the text of the banner that shows when a user is logged out.
    Remove the logic that differentiated between the regular banner and the
    end of year campaign banner.

--- test/unit/components/donate-banner.test.jsx
+++ test/unit/components/donate-banner.test.jsx
@@ -11,27 +11,12 @@ describe('DonateBannerTest', () => {
     afterEach(() => {
         global.Date.now = realDateNow;
     });
-    test('Testing 2024 EOY campaign message', () => {
-        global.Date.now = () => new Date(2024, 11, 16).getTime();
-        const component = mountWithIntl(
-            <DonateTopBanner />
-        );
-
-        expect(component.find('div.donate-banner').exists()).toEqual(true);
-        expect(component.find('p.donate-text').exists()).toEqual(true);
-        expect(component.find('FormattedMessage[id="donatebanner.eoyCampaign"]').exists()).toEqual(true);
-        expect(component.find('FormattedMessage[id="donatebanner.askSupport"]').exists()).toEqual(false);
-
-    });
-    test('testing default message comes back after January 9, 2025', () => {
-        // Date after Scratch week
-        global.Date.now = () => new Date(2025, 0, 10).getTime();
+    test('testing default message', () => {
         const component = mountWithIntl(
             <DonateTopBanner />
         );
         expect(component.find('div.donate-banner').exists()).toEqual(true);
         expect(component.find('p.donate-text').exists()).toEqual(true);
         expect(component.find('FormattedMessage[id="donatebanner.askSupport"]').exists()).toEqual(true);
-        expect(component.find('FormattedMessage[id="donatebanner.eoyCampaign"]').exists()).toEqual(false);
     });
 });
