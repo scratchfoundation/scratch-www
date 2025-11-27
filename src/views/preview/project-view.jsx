8443297a17285a7e8c74234526267544d83d9023
Kaloyan Manolov <kmanolov3@gmail.com>
2025-11-27 12:43:20 +0200

    feat: pass isFetchingUserData param to gui

--- src/views/preview/project-view.jsx
+++ src/views/preview/project-view.jsx
@@ -1254,6 +1254,7 @@ class Preview extends React.Component {
                                     enableCommunity={this.props.enableCommunity}
                                     hasActiveMembership={this.props.hasActiveMembership}
                                     hasCloudPermission={this.props.isScratcher}
+                                    isFetchingUserData={!this.props.hasFetchedSession}
                                     isShared={this.props.isShared}
                                     isTotallyNormal={this.props.isTotallyNormal}
                                     projectHost={this.props.projectHost}
@@ -1353,6 +1354,7 @@ Preview.propTypes = {
     handleSeeAllComments: PropTypes.func,
     handleToggleLoginOpen: PropTypes.func,
     handleUpdateProjectThumbnail: PropTypes.func,
+    hasFetchedSession: PropTypes.bool,
     isAdmin: PropTypes.bool,
     isEditable: PropTypes.bool,
     feedback: PropTypes.object,
@@ -1436,6 +1438,7 @@ const mapStateToProps = state => {
         Object.keys(state.session.session.user).length > 0;
     const isLoggedIn = state.session.status === sessionActions.Status.FETCHED &&
         userPresent;
+    const hasFetchedSession = state.session.status === sessionActions.Status.FETCHED;
     const isAdmin = isLoggedIn && state.session.session.permissions.admin;
     const author = projectInfoPresent && state.preview.projectInfo.author;
     const authorPresent = author && Object.keys(state.preview.projectInfo.author).length > 0;
@@ -1453,7 +1456,7 @@ const mapStateToProps = state => {
         state.session.session.flags.confirm_email_banner;
     const isTotallyNormal = state.session.session.flags && selectIsTotallyNormal(state);
     const userUsesParentEmail = state.session.session.flags && state.session.session.flags.with_parent_email;
-    const hasActiveMembership = state.session.session.flags && !!state.session.session.flags.has_active_membership;
+    const hasActiveMembership = state.session.session.flags && state.session.session.flags.has_active_membership;
 
     // if we don't have projectInfo, assume it's shared until we know otherwise
     const isShared = !projectInfoPresent || state.preview.projectInfo.is_published;
@@ -1481,6 +1484,7 @@ const mapStateToProps = state => {
         feedback: state.feedback,
         fullScreen: state.scratchGui.mode.isFullScreen,
         hasActiveMembership,
+        hasFetchedSession,
         // project is editable iff logged in user is the author of the project, or
         // logged in user is an admin.
         isEditable: isEditable,
