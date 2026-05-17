303540b044510663ac2a56d0f9d5771a7e1ad999
John <roblox888i12345@gmail.com>
2026-05-17 16:01:48 -0400

    Import StudentDeactivationBanner and update display logic
    Add StudentDeactivationBanner import and condition for display

--- src/components/page/www/page.jsx
+++ src/components/page/www/page.jsx
@@ -9,6 +9,7 @@ const ErrorBoundary = require('../../errorboundary/errorboundary.jsx');
 const PrivacyBanner = require('../../privacy-banner/privacy-banner.jsx');
 const TosModal = require('../../modal/tos/modal.jsx');
 const ParentalConsentView = require('../../../views/parental-consent/parental-consent-view.jsx');
+const StudentDeactivationBanner = require('../../student-deactivation-banner/student-deactivation-banner.jsx');
 const ALLOWED_PAGES = ['community_guidelines','contact_us'];
 const today = new Date();
 const semi = today.getDate() === 1 && today.getMonth() === 3;
@@ -43,6 +44,8 @@ const Page = ({
         !user.acceptedTermsOfService &&
         !isAllowedPage &&
         !shouldDisplayTosModal;
+
+    const shouldDisplayStudentDeactivationBanner = !!(user.isStudent && user);
     
     return (
         <ErrorBoundary componentName="Page">
