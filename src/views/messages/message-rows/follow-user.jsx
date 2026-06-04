ab1833a653790aa74e61ab04780d5a8cdcef24e3
John <roblox888i12345@gmail.com>
2026-06-04 14:35:40 -0400

    chore: Remove MembershipLabel from FollowUserMessage
    Removed MembershipLabel component from FollowUserMessage.

--- src/views/messages/message-rows/follow-user.jsx
+++ src/views/messages/message-rows/follow-user.jsx
@@ -4,7 +4,6 @@ const PropTypes = require('prop-types');
 const React = require('react');
 
 const SocialMessage = require('../../../components/social-message/social-message.jsx');
-const MembershipLabel = require('../../../components/membership-label/membership-label.jsx');
 
 const FollowUserMessage = props => (
     <SocialMessage
@@ -27,7 +26,6 @@ const FollowUserMessage = props => (
                         >
                             {props.followerUsername}
                         </a>
-                        {!!props.followerLabel && <MembershipLabel labelType={props.followerLabel} />}
                     </span>
                 )
             }}
